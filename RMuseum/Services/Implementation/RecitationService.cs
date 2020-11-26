b2cda2e7fe5c4218e981060410e520f15586fa8e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-11-26 10:19:59 +0330

    #42 AudioOrder fix

--- RMuseum/Services/Implementation/RecitationService.cs
+++ RMuseum/Services/Implementation/RecitationService.cs
@@ -818,7 +818,7 @@ namespace RMuseum.Services.Implementation
                                                         GanjoorPostId = audio.PoemId,
                                                         OwnerId = session.UseId,
                                                         GanjoorAudioId = 1 + await context.Recitations.OrderByDescending(a => a.GanjoorAudioId).Select(a => a.GanjoorAudioId).FirstOrDefaultAsync(),
-                                                        AudioOrder = 1 + await context.Recitations.Where(a => a.GanjoorPostId == audio.PoemId).OrderByDescending(a => a.GanjoorAudioId).Select(a => a.GanjoorAudioId).FirstOrDefaultAsync(),
+                                                        AudioOrder = 1 + await context.Recitations.Where(a => a.GanjoorPostId == audio.PoemId).OrderByDescending(a => a.GanjoorAudioId).Select(a => a.AudioOrder).FirstOrDefaultAsync(),
                                                         FileNameWithoutExtension = fileNameWithoutExtension,
                                                         SoundFilesFolder = currentTargetFolder,
                                                         AudioTitle = string.IsNullOrEmpty(audio.PoemTitle) ? audio.Description : audio.PoemTitle,
