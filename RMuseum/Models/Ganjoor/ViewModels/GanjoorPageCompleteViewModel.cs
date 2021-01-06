f760e7e51e3719a61e85501e471a3236a451c072
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-01-06 09:47:59 +0330

    #59 added GanjoorPageCompleteViewModel

---
+++ RMuseum/Models/Ganjoor/ViewModels/GanjoorPageCompleteViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace RMuseum.Models.Ganjoor.ViewModels
+{
+    /// <summary>
+    /// Ganjoor Page View Model
+    /// </summary>
+    public class GanjoorPageCompleteViewModel
+    {
+        /// <summary>
+        /// Ganjoor Page Type
+        /// </summary>
+        public GanjoorPageType GanjoorPageType { get; set; }
+
+        /// <summary>
+        /// title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// cat + parent cats title + title
+        /// </summary>
+        public string FullTitle { get; set; }
+
+        /// <summary>
+        /// url => slug
+        /// </summary>
+        public string UrlSlug { get; set; }
+
+        /// <summary>
+        /// sample: /hafez/ghazal/sh1
+        /// </summary>
+        public string FullUrl { get; set; }
+
+        /// <summary>
+        /// Html Text
+        /// </summary>
+        public string HtmlText { get; set; }
+
+        /// <summary>
+        /// Poet or Cat
+        /// </summary>
+        public GanjoorPoetCompleteViewModel PoetOrCat { get; set; }
+
+        /// <summary>
+        /// Poem
+        /// </summary>
+        public GanjoorPoemCompleteViewModel Poem { get; set; }
+
+        /// <summary>
+        /// Second Poet
+        /// </summary>
+        public GanjoorPoetViewModel SecondPoet { get; set; }
+    }
+}
