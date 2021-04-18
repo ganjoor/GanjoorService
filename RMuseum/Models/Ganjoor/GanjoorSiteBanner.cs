26dd8ba482155841d56f264e0d9453167ca35815
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-04-18 18:25:06 +0430

    #106 GanjoorSiteBanner

---
+++ RMuseum/Models/Ganjoor/GanjoorSiteBanner.cs
@@ -0,0 +1,41 @@
+﻿using RSecurityBackend.Models.Image;
+using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// ganjoor.net site banner
+    /// </summary>
+    public class GanjoorSiteBanner
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// banner image
+        /// </summary>
+        public virtual RImage RImage { get; set; }
+
+        /// <summary>
+        /// image id
+        /// </summary>
+        public Guid? RImageId { get; set; }
+
+        /// <summary>
+        /// alternate text
+        /// </summary>
+        public string AlternateText { get; set; }
+
+        /// <summary>
+        /// target url
+        /// </summary>
+        public string TargetUrl { get; set; }
+
+        /// <summary>
+        /// active
+        /// </summary>
+        public bool Active { get; set; }
+    }
+}
