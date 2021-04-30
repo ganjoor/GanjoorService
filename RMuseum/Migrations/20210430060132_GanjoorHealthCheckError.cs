3f7aa6a3b289b332b901d17628f69de7a0e7075e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-04-30 10:33:14 +0430

    #109 GanjoorHealthCheckError db migration

---
+++ RMuseum/Migrations/20210430060132_GanjoorHealthCheckError.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RMuseum.Migrations
+{
+    public partial class GanjoorHealthCheckError : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GanjoorHealthCheckErrors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReferrerPageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BrokenLink = table.Column<bool>(type: "bit", nullable: false),
+                    MulipleTargets = table.Column<bool>(type: "bit", nullable: false),
+                    TargetUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GanjoorHealthCheckErrors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GanjoorHealthCheckErrors");
+        }
+    }
+}
