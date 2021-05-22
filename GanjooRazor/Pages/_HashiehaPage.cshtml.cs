e7e50520df3fe5b8df12ec69c701bcca6f2ec51e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-05-22 16:43:24 +0430

    improving all comments page

--- GanjooRazor/Pages/_HashiehaPage.cshtml.cs
+++ GanjooRazor/Pages/_HashiehaPage.cshtml.cs
@@ -78,8 +78,8 @@ namespace GanjooRazor.Pages
             foreach (var comment in comments)
             {
                 string commentAuthorLink = comment.UserId == null ? comment.AuthorName : $"<a href=\"/hashieha/?userid={comment.UserId}\">{comment.AuthorName}</a>";
-                htmlText += $"<blockquote>{comment.HtmlComment}{Environment.NewLine}" +
-                    $"<p>{commentAuthorLink} <small>در {comment.CommentDate.ToFriendlyPersianDateTextify()}</small> دربارهٔ <a href=\"{comment.Poem.UrlSlug}#comment-{comment.Id}\">{comment.Poem.Title}</a>" +
+                htmlText += $"<p>{commentAuthorLink} <small>در {comment.CommentDate.ToFriendlyPersianDateTextify()}</small> دربارهٔ <a href=\"{comment.Poem.UrlSlug}#comment-{comment.Id}\">{comment.Poem.Title}</a>:</p>" + 
+                    $"<blockquote>{comment.HtmlComment}{Environment.NewLine}" +
                     $"</blockquote>{Environment.NewLine}<hr />{Environment.NewLine}";
             }
 
