6fc5f955ec6ed65110ed392aa22e0b980cd09595
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-05-31 19:01:33 +0430

    #125 displaying older versions of a page

--- GanjooRazor/Areas/Admin/Pages/PageHistory.cshtml.cs
+++ GanjooRazor/Areas/Admin/Pages/PageHistory.cshtml.cs
@@ -1,4 +1,10 @@
+﻿using GanjooRazor.Utils;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RMuseum.Models.Ganjoor.ViewModels;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace GanjooRazor.Areas.Admin.Pages
 {
@@ -8,8 +14,35 @@ namespace GanjooRazor.Areas.Admin.Pages
         /// last message
         /// </summary>
         public string LastMessage { get; set; }
-        public void OnGet()
+
+        public GanjoorPageSnapshotSummaryViewModel[] OlderVersions { get; set; }
+        public async Task<IActionResult> OnGetAsync()
         {
+            LastMessage = "";
+
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+                    var response = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/page/oldversions/{Request.Query["id"]}");
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        LastMessage = await response.Content.ReadAsStringAsync();
+                    }
+
+                    response.EnsureSuccessStatusCode();
+
+                    OlderVersions = JsonConvert.DeserializeObject<GanjoorPageSnapshotSummaryViewModel[]>(await response.Content.ReadAsStringAsync());
+
+                }
+                else
+                {
+                    LastMessage = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+
+            }
+
+            return Page();
         }
     }
 }
