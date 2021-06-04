4e31c248e3aaf1cc758e8f4b8af1418b9441f5c7
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-06-04 16:13:55 +0430

    #129 ganjoor donation model

---
+++ RMuseum/Models/Accounting/GanjoorDonation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace RMuseum.Models.Accounting
+{
+    /// <summary>
+    /// Ganjoor Donation (based on donation records saved in old html format of https://ganjoor.net/donate/)
+    /// </summary>
+    public class GanjoorDonation
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// date: (donation dates have been collected in different formats, so instead of keeping their dates in a DateTime field I ought to use a string field)
+        /// </summary>
+        public string DateString { get; set; }
+
+        /// <summary>
+        /// record date
+        /// </summary>
+        public DateTime RecordDate { get; set; }
+
+        /// <summary>
+        /// donation amount (0 for old imported records)
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Amount Unit
+        /// </summary>
+        public string Unit { get; set; }
+
+        /// <summary>
+        /// amount string (avoiding to parse different formats of old html text)
+        /// </summary>
+        public string AmountString { get; set; }
+
+        /// <summary>
+        /// donor name
+        /// </summary>
+        public string DonorName { get; set; }
+
+        /// <summary>
+        /// donor link
+        /// </summary>
+        public string DonorLink { get; set; }
+
+        /// <summary>
+        /// remaining
+        /// </summary>
+        public decimal Remaining { get; set; }
+
+        /// <summary>
+        /// expenditure desc
+        /// </summary>
+        public string ExpenditureDesc { get; set; }
+
+        /// <summary>
+        /// record is imported from old HTML text of donation page
+        /// </summary>
+        public bool ImportedRecord { get; set; }
+
+    }
+}
