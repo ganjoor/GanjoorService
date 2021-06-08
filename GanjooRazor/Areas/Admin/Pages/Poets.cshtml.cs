68bf7e49c11f667c5047f8f8af9921b25062197a
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-06-08 20:49:26 +0430

    #128 listing all poets in admin panel

---
+++ GanjooRazor/Areas/Admin/Pages/Poets.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using GanjooRazor.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RMuseum.Models.Ganjoor.ViewModels;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GanjooRazor.Areas.Admin.Pages
+{
+    public class PoetsModel : PageModel
+    {
+        /// <summary>
+        /// last message
+        /// </summary>
+        public string LastMessage { get; set; }
+
+        /// <summary>
+        /// poets
+        /// </summary>
+        public GanjoorPoetViewModel[] Poets { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            LastMessage = "";
+
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+                    var response = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poets/all");
+                    response.EnsureSuccessStatusCode();
+
+                    Poets = JsonConvert.DeserializeObject<GanjoorPoetViewModel[]>(await response.Content.ReadAsStringAsync());
+
+                }
+                else
+                {
+                    LastMessage = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+
+            }
+
+            return Page();
+        }
+    }
+}
