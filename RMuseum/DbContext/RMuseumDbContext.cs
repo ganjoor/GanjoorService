2dae0f0fa50fa5245d8ab0340fdb6f5b90383e2d
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-07-01 20:10:56 +0430

    #169 removing db migration

--- RMuseum/DbContext/RMuseumDbContext.cs
+++ RMuseum/DbContext/RMuseumDbContext.cs
@@ -22,7 +22,7 @@ namespace RMuseum.DbContext
     {
         public RMuseumDbContext(DbContextOptions<RMuseumDbContext> options) : base(options) 
         {
-            Database.Migrate();
+            //Database.Migrate();
         }
 
         /// <summary>
