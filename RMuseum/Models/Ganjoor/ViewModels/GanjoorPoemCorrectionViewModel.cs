4397f79d2ed2d4489e91aa146d70c3c58cc173e4
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-07-28 19:57:02 +0430

    #188 adding GanjoorPoemCorrectionViewModel

---
+++ RMuseum/Models/Ganjoor/ViewModels/GanjoorPoemCorrectionViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace RMuseum.Models.Ganjoor.ViewModels
+{
+    /// <summary>
+    /// poem correction view model
+    /// </summary>
+    public class GanjoorPoemCorrectionViewModel
+    {
+        /// <summary>
+        /// poem Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// modified verses
+        /// </summary>
+        public GanjoorVerseVOrderTextViewModel[] VerseOrderText { get; set; }
+
+        /// <summary>
+        /// rhythm
+        /// </summary>
+        public string Rhythm { get; set; }
+
+        /// <summary>
+        /// note
+        /// </summary>
+        public string Note { get; set; }
+    }
+
+    /// <summary>
+    /// Verse Vorder / Text
+    /// </summary>
+    public class GanjoorVerseVOrderTextViewModel
+    {
+        /// <summary>
+        /// verse order
+        /// </summary>
+        public int VORder { get; set; }
+
+        /// <summary>
+        /// text
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
