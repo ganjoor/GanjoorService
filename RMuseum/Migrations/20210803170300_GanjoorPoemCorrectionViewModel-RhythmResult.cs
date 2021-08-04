8420cb809bdee45ed8bb760f8d1930a833e914f9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-08-04 08:08:56 +0430

    #186 adding migration for rhythm review result field and ui for reviewing title and rhythm

---
+++ RMuseum/Migrations/20210803170300_GanjoorPoemCorrectionViewModel-RhythmResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RMuseum.Migrations
+{
+    public partial class GanjoorPoemCorrectionViewModelRhythmResult : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RhythmResult",
+                table: "GanjoorPoemCorrections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RhythmResult",
+                table: "GanjoorPoemCorrections");
+        }
+    }
+}
