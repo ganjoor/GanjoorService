45880c185b542c189b0296436b2f8bbaefb10dcd
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-08-18 21:48:53 +0430

    #190 signup page is nearly complete

---
+++ GanjooRazor/Models/VerifiedSignUpViewModelWithRepPass.cs
@@ -0,0 +1,15 @@
+﻿using RSecurityBackend.Models.Auth.ViewModels;
+
+namespace GanjooRazor.Models
+{
+    /// <summary>
+    /// erifiedSignUpViewModel with PasswordConfirmation
+    /// </summary>
+    public class VerifiedSignUpViewModelWithRepPass : VerifiedSignUpViewModel
+    {
+        /// <summary>
+        /// password confirmation
+        /// </summary>
+        public string PasswordConfirmation { get; set; }
+    }
+}
--- GanjooRazor/Pages/SignUp.cshtml.cs
+++ GanjooRazor/Pages/SignUp.cshtml.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GanjooRazor.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using RMuseum.Models.Auth.Memory;
+using RSecurityBackend.Models.Auth.Memory;
 using RSecurityBackend.Models.Auth.ViewModels;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,9 +52,11 @@ namespace GanjooRazor.Pages
         public string Secret { get; set; }
 
         [BindProperty]
-        public VerifiedSignUpViewModel FinalViewModel { get; set; }
+        public VerifiedSignUpViewModelWithRepPass FinalViewModel { get; set; }
         public async Task<IActionResult> OnGetAsync()
         {
+            if (!string.IsNullOrEmpty(Request.Query["secret"]))
+                return await OnPostPhase2Async(Request.Query["secret"]);
             LoggedIn = !string.IsNullOrEmpty(Request.Cookies["Name"]);
             LastError = "";
             SignupPhase1 = true;
@@ -73,7 +80,7 @@ namespace GanjooRazor.Pages
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(UnverifiedSignUpViewModel SignUpViewModel)
+        public async Task<IActionResult> OnPostPhase1Async(UnverifiedSignUpViewModel signUpViewModel)
         {
             LoggedIn = !string.IsNullOrEmpty(Request.Cookies["Name"]);
             LastError = "";
@@ -85,6 +92,24 @@ namespace GanjooRazor.Pages
             if(!response.IsSuccessStatusCode)
             {
                 LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+
+                response = await _httpClient.GetAsync($"{APIRoot.Url}/api/users/captchaimage");
+                response.EnsureSuccessStatusCode();
+
+                ModelState.Clear();
+
+                SignUpViewModel = new UnverifiedSignUpViewModel()
+                {
+                    ClientAppName = signUpViewModel.ClientAppName,
+                    Language = signUpViewModel.Language,
+                    CallbackUrl = signUpViewModel.CallbackUrl,
+                    Email = signUpViewModel.Email
+                };
+
+                SignUpViewModel.CaptchaImageId = JsonConvert.DeserializeObject<Guid>(await response.Content.ReadAsStringAsync());
+                CaptchaImageUrl = $"{APIRoot.InternetUrl}/api/rimages/{SignUpViewModel.CaptchaImageId}.jpg";
+
+                
                 return Page();
             }
             SignupPhase1 = false;
@@ -94,7 +119,7 @@ namespace GanjooRazor.Pages
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(string Secret)
+        public async Task<IActionResult> OnPostPhase2Async(string Secret)
         {
             LoggedIn = !string.IsNullOrEmpty(Request.Cookies["Name"]);
             LastError = "";
@@ -109,19 +134,101 @@ namespace GanjooRazor.Pages
                 return Page();
             }
 
-            FinalViewModel = new VerifiedSignUpViewModel()
+            FinalViewModel = new VerifiedSignUpViewModelWithRepPass()
             {
                 Secret = Secret,
                 Email = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync()),
                 FirstName = "",
                 SureName = "",
-                Password = ""
+                Password = "",
+                PasswordConfirmation = ""
             };
 
             SignupVerifyEmailPhase = false;
             SignupFinalPhase = true;
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostPhase3Async()
+        {
+            LoggedIn = !string.IsNullOrEmpty(Request.Cookies["Name"]);
+            LastError = "";
+            SignupPhase1 = false;
+            SignupVerifyEmailPhase = false;
+            SignupFinalPhase = true;
+
+            if(FinalViewModel.Password != FinalViewModel.PasswordConfirmation)
+            {
+                LastError = "گذرواژه و تکرار آن یکی نیستند.";
+                return Page();
+            }
+
+            VerifiedSignUpViewModel postViewModel = new VerifiedSignUpViewModel()
+            {
+                Email = FinalViewModel.Email,
+                Secret = FinalViewModel.Secret,
+                FirstName = FinalViewModel.FirstName,
+                SureName = FinalViewModel.SureName,
+                Password = FinalViewModel.Password
+            };
+
+            var response = await _httpClient.PostAsync($"{APIRoot.Url}/api/users/finalizesignup", new StringContent(JsonConvert.SerializeObject(postViewModel), Encoding.UTF8, "application/json"));
+            if (!response.IsSuccessStatusCode)
+            {
+                LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                return Page();
+            }
+
+
+            LoginViewModel loginViewModel = new LoginViewModel()
+            {
+                ClientAppName = "وبگاه گنجور",
+                Language = "fa-IR",
+                Username = postViewModel.Email,
+                Password = postViewModel.Password
+            };
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(loginViewModel), Encoding.UTF8, "application/json");
+            var loginUrl = $"{APIRoot.Url}/api/users/login";
+            response = await _httpClient.PostAsync(loginUrl, stringContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                return Page();
+            }
+
+            LoggedOnUserModel loggedOnUser = JsonConvert.DeserializeObject<LoggedOnUserModel>(await response.Content.ReadAsStringAsync());
+
+            var cookieOption = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(365),
+            };
+
+            Response.Cookies.Append("UserId", loggedOnUser.User.Id.ToString(), cookieOption);
+            Response.Cookies.Append("SessionId", loggedOnUser.SessionId.ToString(), cookieOption);
+            Response.Cookies.Append("Token", loggedOnUser.Token, cookieOption);
+            Response.Cookies.Append("Username", loggedOnUser.User.Username, cookieOption);
+            Response.Cookies.Append("Name", $"{loggedOnUser.User.FirstName} {loggedOnUser.User.SureName}", cookieOption);
+            Response.Cookies.Append("NickName", $"{loggedOnUser.User.NickName}", cookieOption);
+
+            bool canEditContent = false;
+            var ganjoorEntity = loggedOnUser.SecurableItem.Where(s => s.ShortName == RMuseumSecurableItem.GanjoorEntityShortName).SingleOrDefault();
+            if (ganjoorEntity != null)
+            {
+                var op = ganjoorEntity.Operations.Where(o => o.ShortName == SecurableItem.ModifyOperationShortName).SingleOrDefault();
+                if (op != null)
+                {
+                    canEditContent = op.Status;
+                }
+            }
+
+            Response.Cookies.Append("CanEdit", canEditContent.ToString(), cookieOption);
+
+
+
+            return Redirect($"{_configuration["SiteUrl"]}/User");
         }
     }
 }
