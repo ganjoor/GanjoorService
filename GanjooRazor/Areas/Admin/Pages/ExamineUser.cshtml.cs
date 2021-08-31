5f6a638b826ea185f5ab8ecb0de6ee14b13d16ff
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-08-31 19:20:06 +0430

    #195 displaying user info in the ExamineUser page

--- GanjooRazor/Areas/Admin/Pages/ExamineUser.cshtml.cs
+++ GanjooRazor/Areas/Admin/Pages/ExamineUser.cshtml.cs
@@ -1,16 +1,47 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using GanjooRazor.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RSecurityBackend.Models.Auth.ViewModels;
 
 namespace GanjooRazor.Areas.Admin.Pages
 {
     public class ExamineUserModel : PageModel
     {
-        public void OnGet()
+
+        public PublicRAppUser UserInfo { get; set; }
+
+        /// <summary>
+        /// Last Error
+        /// </summary>
+        public string LastError { get; set; }
+
+        public async Task OnGetAsync()
         {
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+                    var userInfoResponse = await secureClient.GetAsync($"{APIRoot.Url}/api/users/{Request.Query["id"]}");
+                    if (userInfoResponse.IsSuccessStatusCode)
+                    {
+                        UserInfo = JsonConvert.DeserializeObject<PublicRAppUser>(await userInfoResponse.Content.ReadAsStringAsync());
+                    }
+                    else
+                    {
+                        LastError = await userInfoResponse.Content.ReadAsStringAsync();
+                    }
+                }
+                else
+                {
+                    LastError = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+            }
         }
     }
 }
