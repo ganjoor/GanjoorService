9bd85be57f0f5a3778bb7fc9aa16592db238082a
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-09-14 19:56:34 +0430

    #203 edit language

---
+++ GanjooRazor/Areas/User/Pages/LanguageEdit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GanjooRazor.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RMuseum.Models.Auth.Memory;
+using RMuseum.Models.Ganjoor;
+
+namespace GanjooRazor.Areas.User.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class LanguageEditModel : PageModel
+    {
+        [BindProperty]
+        public GanjoorLanguage Language { get; set; }
+
+        public string LastMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            LastMessage = "";
+            await GanjoorSessionChecker.ApplyPermissionsToViewData(Request, Response, ViewData);
+            if (!ViewData.ContainsKey($"{RMuseumSecurableItem.GanjoorEntityShortName}-{RMuseumSecurableItem.Translations}"))
+            {
+                LastMessage = "شما به این بخش دسترسی ندارید.";
+                return Page();
+            }
+
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+                    HttpResponseMessage response = await secureClient.GetAsync($"{APIRoot.Url}/api/translations/languages/{Request.Query["id"]}");
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        LastMessage = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                        return Page();
+                    }
+                    response.EnsureSuccessStatusCode();
+
+                    Language = JsonConvert.DeserializeObject<GanjoorLanguage>(await response.Content.ReadAsStringAsync());
+
+                }
+                else
+                {
+                    LastMessage = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+            }
+
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            using (HttpClient secureClient = new HttpClient())
+            {
+                await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response);
+                HttpResponseMessage response = await secureClient.PutAsync($"{APIRoot.Url}/api/translations/languages", new StringContent(JsonConvert.SerializeObject(Language), Encoding.UTF8, "application/json"));
+                if (!response.IsSuccessStatusCode)
+                {
+                    LastMessage = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                    return Page();
+                }
+
+                response.EnsureSuccessStatusCode();
+
+                return Redirect("/User/Languages");
+            }
+        }
+    }
+}
