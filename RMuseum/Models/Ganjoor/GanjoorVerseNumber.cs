45172f6928925d4e33895894f3488b7127d4cf61
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-10-03 11:34:57 +0330

    #217 adding GanjoorVerseNumber model

---
+++ RMuseum/Models/Ganjoor/GanjoorVerseNumber.cs
@@ -0,0 +1,35 @@
+﻿namespace RMuseum.Models.Ganjoor
+{
+    public class GanjoorVerseNumber
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Numbering schema Id
+        /// </summary>
+        public int NumberingId { get; set; }
+
+        /// <summary>
+        /// Numbering schema
+        /// </summary>
+        public GanjoorNumbering Numbering { get; set; }
+
+        /// <summary>
+        /// verse id
+        /// </summary>
+        public int VerseId { get; set; }
+
+        /// <summary>
+        /// verse
+        /// </summary>
+        public GanjoorVerse Verse { get; set; }
+
+        /// <summary>
+        /// number
+        /// </summary>
+        public int Number { get; set; }
+    }
+}
