290b8fa2123e18dd5d5563b8d515d29afcc24800
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-10-13 19:55:53 +0330

    #221 removing bookmarks verse id dependency

--- RMuseum/Migrations/20211013162354_GanjoorUserBookmark-RemoveVerseIdDep.Designer.cs
+++ RMuseum/Migrations/20211013162354_GanjoorUserBookmark-RemoveVerseIdDep.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RMuseum.DbContext;
 
 namespace RMuseum.Migrations
 {
     [DbContext(typeof(RMuseumDbContext))]
-    partial class RMuseumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211013162354_GanjoorUserBookmark-RemoveVerseIdDep")]
+    partial class GanjoorUserBookmarkRemoveVerseIdDep
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RMuseum/Migrations/20211013162354_GanjoorUserBookmark-RemoveVerseIdDep.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RMuseum.Migrations
+{
+    public partial class GanjoorUserBookmarkRemoveVerseIdDep : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GanjoorUserBookmarks_UserId_PoemId_VerseId",
+                table: "GanjoorUserBookmarks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VerseId",
+                table: "GanjoorUserBookmarks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GanjoorUserBookmarks_UserId_PoemId_VerseId",
+                table: "GanjoorUserBookmarks",
+                columns: new[] { "UserId", "PoemId", "VerseId" },
+                unique: true,
+                filter: "[VerseId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GanjoorUserBookmarks_UserId_PoemId_VerseId",
+                table: "GanjoorUserBookmarks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VerseId",
+                table: "GanjoorUserBookmarks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GanjoorUserBookmarks_UserId_PoemId_VerseId",
+                table: "GanjoorUserBookmarks",
+                columns: new[] { "UserId", "PoemId", "VerseId" },
+                unique: true);
+        }
+    }
+}
