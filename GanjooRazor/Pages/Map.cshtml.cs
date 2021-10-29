ff8483eb838846a4c38fe650ad8cc75e495390d6
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-10-29 19:32:45 +0330

    #233 displaying poet birth places on the map

--- GanjooRazor/Pages/Map.cshtml.cs
+++ GanjooRazor/Pages/Map.cshtml.cs
@@ -1,16 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using RMuseum.Models.Ganjoor.ViewModels;
 
 namespace GanjooRazor.Pages
 {
     public class MapModel : PageModel
     {
-        public void OnGet()
+        /// <summary>
+        /// Poets
+        /// </summary>
+        public List<GanjoorPoetViewModel> PoetsWithBirthPlaces { get; set; }
+
+        private async Task preparePoets()
         {
+            var response = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poets");
+            response.EnsureSuccessStatusCode();
+            var poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+
+            PoetsWithBirthPlaces = poets.Where(p => !string.IsNullOrEmpty(p.BirthPlace)).ToList();
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await preparePoets();
+            return Page();
+        }
+
+        /// <summary>
+        /// HttpClient instance
+        /// </summary>
+        private readonly HttpClient _httpClient;
+
+        /// <summary>
+        /// memory cache
+        /// </summary>
+        private readonly IMemoryCache _memoryCache;
+
+        /// <summary>
+        /// configration file reader (appsettings.json)
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        public MapModel(IConfiguration configuration,
+            HttpClient httpClient,
+            IMemoryCache memoryCache
+            )
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+            _memoryCache = memoryCache;
         }
     }
 }
