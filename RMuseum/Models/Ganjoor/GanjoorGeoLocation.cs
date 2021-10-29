7c10bb956526d049671c5173e410680ce32f31dd
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-10-29 08:52:20 +0330

    #233 adding GanjoorGeoLocation model

---
+++ RMuseum/Models/Ganjoor/GanjoorGeoLocation.cs
@@ -0,0 +1,26 @@
+﻿using NetTopologySuite.Geometries;
+
+
+namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// Geo Locations (Cities) referred by Ganjoor Metadata
+    /// </summary>
+    public class GanjoorGeoLocation
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// geo location
+        /// </summary>
+        public Point Location { get; set; }
+    }
+}
