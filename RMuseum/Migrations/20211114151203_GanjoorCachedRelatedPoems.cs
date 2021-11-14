49efc1ab86f54d79bef9d0adcbd9a57234305a7a
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-11-14 18:43:37 +0330

    #241 adding database migration

---
+++ RMuseum/Migrations/20211114151203_GanjoorCachedRelatedPoems.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RMuseum.Migrations
+{
+    public partial class GanjoorCachedRelatedPoems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GanjoorCachedRelatedPoems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PoemId = table.Column<int>(type: "int", nullable: false),
+                    RelationOrder = table.Column<int>(type: "int", nullable: false),
+                    PoetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PoetImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HtmlExcerpt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PoetMorePoemsLikeThisCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GanjoorCachedRelatedPoems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GanjoorCachedRelatedPoems_GanjoorPoems_PoemId",
+                        column: x => x.PoemId,
+                        principalTable: "GanjoorPoems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GanjoorCachedRelatedPoems_PoemId_RelationOrder",
+                table: "GanjoorCachedRelatedPoems",
+                columns: new[] { "PoemId", "RelationOrder" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GanjoorCachedRelatedPoems");
+        }
+    }
+}
