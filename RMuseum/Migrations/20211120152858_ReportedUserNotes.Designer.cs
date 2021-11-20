da487be764254adaee6748cc9cf06d2d3109a9ef
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-11-20 19:05:33 +0330

    #246 adding database migration

--- RMuseum/Migrations/20211120152858_ReportedUserNotes.Designer.cs
+++ RMuseum/Migrations/20211120152858_ReportedUserNotes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RMuseum.DbContext;
 
@@ -11,9 +12,10 @@ using RMuseum.DbContext;
 namespace RMuseum.Migrations
 {
     [DbContext(typeof(RMuseumDbContext))]
-    partial class RMuseumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211120152858_ReportedUserNotes")]
+    partial class ReportedUserNotes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RMuseum/Migrations/20211120152858_ReportedUserNotes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class ReportedUserNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportedUserNotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NoteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReasonText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReporterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportedUserNotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReportedUserNotes_AspNetUsers_ReporterId",
+                        column: x => x.ReporterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ReportedUserNotes_UserNotes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "UserNotes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportedUserNotes_NoteId",
+                table: "ReportedUserNotes",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportedUserNotes_ReporterId",
+                table: "ReportedUserNotes",
+                column: "ReporterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportedUserNotes");
+        }
+    }
+}
