95a659c8bdfd2287601378a881af827c5b1235c0
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-11-29 21:06:19 +0330

    #250 adding recitation meta data to new UI

--- GanjooRazor/Pages/_AudioPlayerPartial.cshtml.cs
+++ GanjooRazor/Pages/_AudioPlayerPartial.cshtml.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using RMuseum.Models.GanjoorAudio.ViewModels;
 
 namespace GanjooRazor.Pages
@@ -6,5 +6,39 @@ namespace GanjooRazor.Pages
     public class _AudioPlayerPartialModel : PageModel
     {
         public PublicRecitationViewModel[] Recitations { get; set; }
+
+        public string getAudioDesc(PublicRecitationViewModel recitation, bool contributionLink = false)
+        {
+            string audiodesc = "به خوانش ";
+            if (!string.IsNullOrEmpty(recitation.AudioArtistUrl))
+            {
+                audiodesc += $"<a href='{recitation.AudioArtistUrl}'>{recitation.AudioArtist}</a>";
+            }
+            else
+            {
+                audiodesc += $"{recitation.AudioArtist}";
+            }
+
+            if (!string.IsNullOrEmpty(recitation.AudioSrc))
+            {
+                if (!string.IsNullOrEmpty(recitation.AudioSrcUrl))
+                {
+                    audiodesc += $" <a href='{recitation.AudioSrcUrl}'>{recitation.AudioSrc}</a>";
+                }
+                else
+                {
+                    audiodesc += $" {recitation.AudioSrc}";
+                }
+            }
+
+            audiodesc += $" <small><a href='/AudioClip/?a={recitation.Id}' onclick='wpopen(this.href); return false' class='comments-link' title='دریافت'>(دریافت)</a></small>";
+
+            if (contributionLink)
+            {
+                audiodesc += "<br /> <small>می‌خواهید شما بخوانید؟ <a href='http://ava.ganjoor.net/about/'>اینجا</a> را ببینید.</small>";
+            }
+
+            return audiodesc;
+        }
     }
 }
