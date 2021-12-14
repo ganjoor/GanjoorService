2c1114c331823f7d6bc4dda74907103575df65cb
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-12-14 20:12:38 +0330

    adding GanjoorVerseVOrderText-CoupletIndex migration to fix recent issues

---
+++ RMuseum/Migrations/20211214161601_GanjoorVerseVOrderText-CoupletIndex.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class GanjoorVerseVOrderTextCoupletIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CoupletIndex",
+                table: "GanjoorVerseVOrderText",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoupletIndex",
+                table: "GanjoorVerseVOrderText");
+        }
+    }
+}
