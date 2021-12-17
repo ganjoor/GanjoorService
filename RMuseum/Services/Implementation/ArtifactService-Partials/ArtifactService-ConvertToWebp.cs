5da8dda866e850bdb866eeca9af7ecaf2546d8ea
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-12-17 11:54:02 +0330

    #262 skeleton for the method to batch convert museum images to webp

---
+++ RMuseum/Services/Implementation/ArtifactService-Partials/ArtifactService-ConvertToWebp.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMuseum.DbContext;
+using RSecurityBackend.Models.Generic;
+using RSecurityBackend.Services.Implementation;
+using System;
+
+namespace RMuseum.Services.Implementation
+{
+
+    /// <summary>
+    /// IArtifactService implementation
+    /// </summary>
+    public partial class ArtifactService : IArtifactService
+    {
+        /// <summary>
+        /// convert original images to webp
+        /// </summary>
+        /// <returns></returns>
+        public RServiceResult<bool> StartConvertingOriginalImagesToWebp()
+        {
+            _backgroundTaskQueue.QueueBackgroundWorkItem
+                        (
+                        async token =>
+                        {
+                            using (RMuseumDbContext context = new RMuseumDbContext(new DbContextOptions<RMuseumDbContext>())) //this is long running job, so _context might be already been freed/collected by GC
+                            {
+                                LongRunningJobProgressServiceEF jobProgressServiceEF = new LongRunningJobProgressServiceEF(context);
+                                var job = (await jobProgressServiceEF.NewJob($"Converting Original Images To Webp", "Query data")).Result;
+                                try
+                                {
+                                    await jobProgressServiceEF.UpdateJob(job.Id, 100, "", true);
+                                }
+                                catch (Exception exp)
+                                {
+                                    await jobProgressServiceEF.UpdateJob(job.Id, 100, "", false, exp.ToString());
+                                }
+
+                            }
+                        });
+
+            return new RServiceResult<bool>(true);
+        }
+    }
+}
