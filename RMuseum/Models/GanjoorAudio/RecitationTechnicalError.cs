948b9f853a7b48775827d6d266827d486da96e3f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-12-27 19:26:49 +0330

    #117 adding a model for storing recitation problems

---
+++ RMuseum/Models/GanjoorAudio/RecitationTechnicalError.cs
@@ -0,0 +1,28 @@
+﻿namespace RMuseum.Models.GanjoorAudio
+{
+    /// <summary>
+    /// recitation technical error
+    /// </summary>
+    public class RecitationTechnicalError
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Recitation Id
+        /// </summary>
+        public int RecitationId { get; set; }
+
+        /// <summary>
+        /// recitation
+        /// </summary>
+        public Recitation Recitation { get; set; }
+
+        /// <summary>
+        /// problem
+        /// </summary>
+        public string Problem { get; set; }
+    }
+}
