2afe20779a05ce3d8e095aad5db16140eaa55992
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-12-29 21:09:25 +0330

    #268 model for probable poem metre

---
+++ RMuseum/Models/Ganjoor/GanjoorPoemProbableMetre.cs
@@ -0,0 +1,28 @@
+﻿namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// Probable Prosody Information for a poem
+    /// </summary>
+    public class GanjoorPoemProbableMetre
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// poem id
+        /// </summary>
+        public int PoemId { get; set; }
+
+        /// <summary>
+        /// poem
+        /// </summary>
+        public GanjoorPoem Poem { get; set; }
+
+        /// <summary>
+        /// metre
+        /// </summary>
+        public string Metre { get; set; }
+    }
+}
