fa1050653dcbdf2d18c3a7197fdbe0f13546904e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-01-08 20:18:16 +0330

    #272 adding GanjoorPoemVisit model

---
+++ RMuseum/Models/Ganjoor/GanjoorPoemVisit.cs
@@ -0,0 +1,41 @@
+﻿using RSecurityBackend.Models.Auth.Db;
+using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// ganjoor user history track item (by his or her choice)
+    /// </summary>
+    public class GanjoorPoemVisit
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// user id (nullable to prevent a db cascade relation)
+        /// </summary>
+        public Guid? UserId { get; set; }
+
+        /// <summary>
+        /// user
+        /// </summary>
+        public virtual RAppUser User { get; set; }
+
+        /// <summary>
+        /// poem id
+        /// </summary>
+        public int PoemId { get; set; }
+
+        /// <summary>
+        /// poem
+        /// </summary>
+        public GanjoorPoem Poem { get; set; }
+
+        /// <summary>
+        /// date/time
+        /// </summary>
+        public DateTime DateTime { get; set; }
+    }
+}
