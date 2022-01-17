8c517fd872051445ab559bf57e14ce420b9f06b5
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-01-17 12:05:10 +0330

    #275 adding models for poet descriptive lines and suggested pictures

---
+++ RMuseum/Models/Ganjoor/GanjoorPoetSuggestedPicture.cs
@@ -0,0 +1,63 @@
+﻿using RMuseum.Models.Artifact;
+using RSecurityBackend.Models.Auth.Db;
+using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// Ganjoor Poet Picture
+    /// </summary>
+    public class GanjoorPoetSuggestedPicture
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Poet Id
+        /// </summary>
+        public int PoetId { get; set; }
+
+        /// <summary>
+        /// Poet
+        /// </summary>
+        public GanjoorPoet Poet { get; set; }
+
+        /// <summary>
+        /// order
+        /// </summary>
+        public int PicOrder { get; set; }
+
+        /// <summary>
+        /// picture id
+        /// </summary>
+        public Guid PictureId { get; set; }
+
+        /// <summary>
+        /// picture
+        /// </summary>
+        public RPictureFile Picture { get; set; }
+
+        /// <summary>
+        /// suggested by id
+        /// </summary>
+        public Guid? SuggestedById { get; set; }
+
+        /// <summary>
+        /// suggested by
+        /// </summary>
+        public virtual RAppUser SuggestedBy { get; set; }
+
+        /// <summary>
+        /// published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// selected
+        /// </summary>
+        public bool ChosenOne { get; set; }
+
+    }
+}
---
+++ RMuseum/Models/Ganjoor/GanjoorPoetSuggestedSpecLine.cs
@@ -0,0 +1,52 @@
+﻿using RSecurityBackend.Models.Auth.Db;
+using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// Ganjoor Poet Specification
+    /// </summary>
+    public class GanjoorPoetSuggestedSpecLine
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Poet Id
+        /// </summary>
+        public int PoetId { get; set; }
+
+        /// <summary>
+        /// Poet
+        /// </summary>
+        public GanjoorPoet Poet { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// suggested by id
+        /// </summary>
+        public Guid? SuggestedById { get; set; }
+
+        /// <summary>
+        /// suggested by
+        /// </summary>
+        public virtual RAppUser SuggestedBy { get; set; }
+
+        /// <summary>
+        /// published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// order
+        /// </summary>
+        public int LineOrder { get; set; }
+
+    }
+}
