9a2cc74920ea95778d029122a7c99946c03ca445
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-01-22 17:56:41 +0330

    docs: #275 adding a page template for photos project

---
+++ GanjooRazor/Pages/Photos.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RMuseum.Models.Ganjoor.ViewModels;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GanjooRazor.Pages
+{
+    public class PhotosModel : PageModel
+    {
+        public string LastError { get; set; }
+
+        public List<GanjoorPoetViewModel> Poets { get; set; }
+
+        private async Task<List<GanjoorPoetViewModel>> _PreparePoets()
+        {
+            var response = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poets");
+            if (!response.IsSuccessStatusCode)
+            {
+                LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                return new List<GanjoorPoetViewModel>();
+            }
+            var poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+
+
+            foreach (var poet in poets)
+            {
+                poet.ImageUrl = $"{APIRoot.InternetUrl}{poet.ImageUrl}";
+            }
+
+            return poets;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ViewData["Title"] = "تصاویر شاعران گنجور";
+            Poets = await _PreparePoets();
+            return Page();
+        }
+
+        /// <summary>
+        /// HttpClient instance
+        /// </summary>
+        private readonly HttpClient _httpClient;
+
+
+        public PhotosModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
