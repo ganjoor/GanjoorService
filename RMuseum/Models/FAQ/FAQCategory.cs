9004e0caac6dd0f67051beb354a9e0f4d693e8d2
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-02-21 19:59:20 +0330

    docs: #285 FAQ database models

---
+++ RMuseum/Models/FAQ/FAQCategory.cs
@@ -0,0 +1,33 @@
+﻿namespace RMuseum.Models.FAQ
+{
+    /// <summary>
+    /// FAQ Category
+    /// </summary>
+    public class FAQCategory
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// order
+        /// </summary>
+        public int CatOrder { get; set; }
+
+        /// <summary>
+        /// description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// published
+        /// </summary>
+        public bool Published { get; set; }
+    }
+}
---
+++ RMuseum/Models/FAQ/FAQItem.cs
@@ -0,0 +1,83 @@
+﻿namespace RMuseum.Models.FAQ
+{
+    /// <summary>
+    /// FAQ Item
+    /// </summary>
+    public class FAQItem
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// question
+        /// </summary>
+        public string Question { get; set; }
+
+        /// <summary>
+        /// excerpt
+        /// </summary>
+        public string AnswerExcerpt { get; set; }
+
+        /// <summary>
+        /// answer
+        /// </summary>
+        public string FullAnswer { get; set; }
+
+        /// <summary>
+        /// pinned
+        /// </summary>
+        public bool Pinned { get; set; }
+
+        /// <summary>
+        /// item order
+        /// </summary>
+        public int PinnedItemOrder { get; set; }
+
+        /// <summary>
+        /// category id
+        /// </summary>
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// category
+        /// </summary>
+        public virtual FAQCategory Category { get; set; }
+
+        /// <summary>
+        /// item order in category
+        /// </summary>
+        public int ItemOrderInCategory { get; set; }
+
+        /// <summary>
+        /// content search
+        /// </summary>
+        public string ContentForSearch { get; set; }
+
+        /// <summary>
+        /// hashtag 1
+        /// </summary>
+        public string HashTag1 { get; set; }
+
+        /// <summary>
+        /// hashtag 2
+        /// </summary>
+        public string HashTag2 { get; set; }
+
+        /// <summary>
+        /// hashtag 3
+        /// </summary>
+        public string HashTag3 { get; set; }
+
+        /// <summary>
+        /// hashtag 4
+        /// </summary>
+        public string HashTag4 { get; set; }
+
+        /// <summary>
+        /// published
+        /// </summary>
+        public bool Published { get; set; }
+    }
+}
