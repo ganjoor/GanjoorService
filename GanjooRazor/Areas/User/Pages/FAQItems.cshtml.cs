9d8dc92eae5f4bf122e74020ed9cc52b3a87e3c5
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-03-02 19:35:40 +0330

    docs: #285 FAQItems page in UI

---
+++ GanjooRazor/Areas/User/Pages/FAQItems.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using GanjooRazor.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RMuseum.Models.Auth.Memory;
+using RMuseum.Models.FAQ;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GanjooRazor.Areas.User.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class FAQItemsModel : PageModel
+    {
+        public string LastMessage { get; set; }
+
+        public FAQCategory[] Categories { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if(string.IsNullOrEmpty(Request.Cookies["Token"]))
+                return Redirect("/");
+
+            LastMessage = "";
+            await GanjoorSessionChecker.ApplyPermissionsToViewData(Request, Response, ViewData);
+            if (!ViewData.ContainsKey($"{RMuseumSecurableItem.FAQEntityShortName}-{RMuseumSecurableItem.ModerateOperationShortName}"))
+            {
+                LastMessage = "شما به این بخش دسترسی ندارید.";
+                return Page();
+            }
+
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+                    HttpResponseMessage response = await secureClient.GetAsync($"{APIRoot.Url}/api/faq/cat/secure");
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        LastMessage = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                        return Page();
+                    }
+
+                    Categories = JsonConvert.DeserializeObject<FAQCategory[]>(await response.Content.ReadAsStringAsync());
+
+                }
+                else
+                {
+                    LastMessage = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+            }
+
+            return Page();
+        }
+    }
+}
