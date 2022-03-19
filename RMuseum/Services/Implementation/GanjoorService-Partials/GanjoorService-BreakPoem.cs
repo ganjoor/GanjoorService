b6709f6aa9142bb41747d5818c82d7113a886a3a
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-03-19 15:43:47 +0330

    docs: #293 implementing some parts of the logic

--- RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-BreakPoem.cs
+++ RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-BreakPoem.cs
@@ -24,19 +24,91 @@ namespace RMuseum.Services.Implementation
         /// <returns></returns>
         public async Task<RServiceResult<int>> BreakPoemAsync(int poemId, int vOrder, Guid userId)
         {
-            var poem = (await GetPoemById(poemId, true, false, true, false, false, false, false, true, true)).Result;
-            if (poem.Next == null)
+            try
             {
-                return await _BreakLastPoemInItsCategoryAsync(poemId, vOrder, userId, poem);
+                var poem = (await GetPoemById(poemId, true, false, true, false, false, false, false, true, true)).Result;
+                var parentPage = await _context.GanjoorPages.AsNoTracking().Where(p => p.GanjoorPageType == GanjoorPageType.CatPage && p.CatId == poem.Category.Cat.Id).SingleAsync();
+                var poemTitleStaticPart = poem.Title.Replace("۰", "").Replace("۱", "1").Replace("۲", "").Replace("۳", "").Replace("۴", "").Replace("۵", "").Replace("۶", "").Replace("۷", "").Replace("۸", "1").Replace("۹", "").Trim();
+                if (poem.Next == null)
+                {
+                    return await _BreakLastPoemInItsCategoryAsync(poemId, vOrder, userId, poem, parentPage, poemTitleStaticPart);
+                }
+                var dbMainPoem = await _context.GanjoorPoems.Include(p => p.GanjoorMetre).Where(p => p.Id == poemId).SingleOrDefaultAsync();
+                var poemList = await _context.GanjoorPoems.AsNoTracking()
+                                                          .Where(p => p.CatId == dbMainPoem.CatId && p.Id > dbMainPoem.Id).OrderBy(p => p.Id).ToListAsync();
+
+                var lastPoemInCategory = poemList.Last();
+
+                if (!int.TryParse(lastPoemInCategory.UrlSlug.Substring("sh".Length), out int newPoemSlugNumber))
+                    return new RServiceResult<int>(-1, $"slug error for the last poem in the category: {lastPoemInCategory.UrlSlug}");
+
+                string nextPoemUrlSluf = $"sh{newPoemSlugNumber + 1}";
+
+                var maxPoemId = await _context.GanjoorPoems.MaxAsync(p => p.Id);
+                if (await _context.GanjoorPages.MaxAsync(p => p.Id) > maxPoemId)
+                    maxPoemId = await _context.GanjoorPages.MaxAsync(p => p.Id);
+                var nextPoemId = 1 + maxPoemId;
+
+                string nextPoemTitle = $"{poemTitleStaticPart} {(newPoemSlugNumber + 1).ToPersianNumbers()}";
+
+                GanjoorPoem dbNewPoem = new GanjoorPoem()
+                {
+                    Id = nextPoemId,
+                    CatId = poem.Category.Cat.Id,
+                    Title = nextPoemTitle,
+                    UrlSlug = nextPoemUrlSluf,
+                    FullTitle = $"{parentPage.FullTitle} » {nextPoemTitle}",
+                    FullUrl = $"{parentPage.FullUrl}/{nextPoemUrlSluf}",
+                    SourceName = poem.SourceName,
+                    SourceUrlSlug = poem.SourceUrlSlug,
+                    Language = dbMainPoem.Language,
+                    MixedModeOrder = dbMainPoem.MixedModeOrder,
+                    Published = dbMainPoem.Published,
+                };
+
+                GanjoorPage dbPoemNewPage = new GanjoorPage()
+                {
+                    Id = nextPoemId,
+                    GanjoorPageType = GanjoorPageType.PoemPage,
+                    Published = true,
+                    PageOrder = -1,
+                    Title = dbNewPoem.Title,
+                    FullTitle = dbNewPoem.FullTitle,
+                    UrlSlug = dbNewPoem.UrlSlug,
+                    FullUrl = dbNewPoem.FullUrl,
+                    HtmlText = dbNewPoem.HtmlText,
+                    PoetId = parentPage.PoetId,
+                    CatId = poem.Category.Cat.Id,
+                    PoemId = nextPoemId,
+                    PostDate = DateTime.Now,
+                    ParentId = parentPage.Id,
+                };
+                _context.GanjoorPoems.Add(dbNewPoem);
+                _context.GanjoorPages.Add(dbPoemNewPage);
+                await _context.SaveChangesAsync();
+
+                var targetPoem = dbNewPoem;
+                var targetPage = dbPoemNewPage;
+
+                //now copy each poem to its next sibling in their category
+
+
+                return new RServiceResult<int>(0, "poem.Next != null");
             }
-            return new RServiceResult<int>(0, "poem.Next != null");
+            catch (Exception exp)
+            {
+                return new RServiceResult<int>(0, exp.ToString());
+            }
+           
         }
 
-            
-        private async Task<RServiceResult<int>> _BreakLastPoemInItsCategoryAsync(int poemId, int vOrder, Guid userId, GanjoorPoemCompleteViewModel poem)
+
+        private async Task<RServiceResult<int>> _BreakLastPoemInItsCategoryAsync(int poemId, int vOrder, Guid userId, GanjoorPoemCompleteViewModel poem, GanjoorPage parentPage, string poemTitleStaticPart)
         {
-            if (poem.Next != null)
-                return new RServiceResult<int>(-1, "poem.Next != null");
+            if (poem.UrlSlug.IndexOf("sh") != 0)
+            {
+                return new RServiceResult<int>(-1, "poem.UrlSlug.IndexOf(\"sh\") != 0");
+            }
 
             try
             {
@@ -66,17 +138,13 @@ namespace RMuseum.Services.Implementation
                 if (!string.IsNullOrEmpty(res.ExceptionString))
                     return new RServiceResult<int>(0, res.ExceptionString);
 
-
-
-
-                if (poem.UrlSlug.IndexOf("sh") != 0)
-                    return new RServiceResult<int>(-1, "poem.UrlSlug.IndexOf(\"sh\") != 0");
+               
 
                 if (!int.TryParse(poem.UrlSlug.Substring("sh".Length), out int slugNumber))
                     return new RServiceResult<int>(-1, $"slug error: {poem.UrlSlug}");
 
                 var poemPage = await _context.GanjoorPages.AsNoTracking().Where(p => p.Id == poem.Id).SingleAsync();
-                var parentPage = await _context.GanjoorPages.AsNoTracking().Where(p => p.GanjoorPageType == GanjoorPageType.CatPage && p.CatId == poem.Category.Cat.Id).SingleAsync();
+                
 
                 string nextPoemUrlSluf = $"sh{slugNumber + 1}";
 
@@ -85,7 +153,7 @@ namespace RMuseum.Services.Implementation
                     maxPoemId = await _context.GanjoorPages.MaxAsync(p => p.Id);
                 var nextPoemId = 1 + maxPoemId;
 
-                string nextPoemTitle = $"بخش {(slugNumber + 1).ToPersianNumbers()}";
+                string nextPoemTitle = $"{poemTitleStaticPart} {(slugNumber + 1).ToPersianNumbers()}";
 
                 GanjoorPoem dbNewPoem = new GanjoorPoem()
                 {
