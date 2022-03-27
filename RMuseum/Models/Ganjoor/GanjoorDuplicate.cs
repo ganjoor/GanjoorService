4f57f6fed642bbf6a63a718eb8842d94401b63c8
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-03-27 14:09:24 +0430

    docs: #302 GanjoorDuplicate model

---
+++ RMuseum/Models/Ganjoor/GanjoorDuplicate.cs
@@ -0,0 +1,38 @@
+﻿namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// Temporary Model contianing duplicated poems information
+    /// </summary>
+    public class GanjoorDuplicate
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// source category id (this category is about to be deleted and redirected)
+        /// </summary>
+        public int SrcCatId { get; set; }
+
+        /// <summary>
+        /// source poem id
+        /// </summary>
+        public int SrcPoemId { get; set; }
+
+        /// <summary>
+        /// source poem
+        /// </summary>
+        public GanjoorPoem SrcPoem { get; set; }
+
+        /// <summary>
+        /// destination poem id
+        /// </summary>
+        public int? DestPoemId { get; set; }
+
+        /// <summary>
+        /// destination poem
+        /// </summary>
+        public virtual GanjoorPoem DestPoem { get; set; }
+    }
+}
