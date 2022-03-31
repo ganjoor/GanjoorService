97f44328f367665fe47d81164df1871a1f6e0e6f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-03-31 10:19:34 +0430

    docs: #305 db migration

---
+++ RMuseum/Migrations/20220331053310_GLinkOrigin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class GLinkOrigin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTextOriginalSource",
+                table: "GanjoorLinks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTextOriginalSource",
+                table: "GanjoorLinks");
+        }
+    }
+}
