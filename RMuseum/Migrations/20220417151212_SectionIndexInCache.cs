1aebce5485720463ab9b78473435ecb29513d9e9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-04-17 19:57:35 +0430

    docs: #306 db migration for new fields

---
+++ RMuseum/Migrations/20220417151212_SectionIndexInCache.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class SectionIndexInCache : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TargetPoemId",
+                table: "GanjoorCachedRelatedSections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TargetSectionIndex",
+                table: "GanjoorCachedRelatedSections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TargetPoemId",
+                table: "GanjoorCachedRelatedSections");
+
+            migrationBuilder.DropColumn(
+                name: "TargetSectionIndex",
+                table: "GanjoorCachedRelatedSections");
+        }
+    }
+}
