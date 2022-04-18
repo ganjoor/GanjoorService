2a8e3bd95b68413e6e8bdf9a7570b8fa3f24145b
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-04-18 21:01:59 +0430

    docs: adding GanjoorPoemFormat

---
+++ RMuseum/Models/Ganjoor/GanjoorPoemFormat.cs
@@ -0,0 +1,129 @@
+﻿namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// poem format
+    /// </summary>
+    public enum GanjoorPoemFormat
+    {
+        /// <summary>
+        /// unknown
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// غزل
+        /// </summary>
+        Ghazal = 1,
+
+        /// <summary>
+        /// قصیده
+        /// </summary>
+        Ghaside = 2,
+
+        /// <summary>
+        /// مثنوی
+        /// </summary>
+        Masnavi = 3,
+
+        /// <summary>
+        /// قطعه
+        /// </summary>
+        Ghete = 4,
+
+        /// <summary>
+        /// رباعی
+        /// </summary>
+        Robaee =  5,
+
+        /// <summary>
+        /// دوبیتی
+        /// </summary>
+        Dobeyti = 6,
+
+        /// <summary>
+        /// غزل/قصیده/قطعه
+        /// </summary>
+        Generic = 7,
+
+        /// <summary>
+        /// ترکیب بند
+        /// </summary>
+        TarkibBand = 8,
+
+        /// <summary>
+        /// تک بیت
+        /// </summary>
+        Takbeyt = 10,
+
+        /// <summary>
+        /// نیمایی
+        /// </summary>
+        Nimaee = 11,
+
+        /// <summary>
+        /// ترکیب بند
+        /// </summary>
+        TarjeeBand = 16,
+
+        /// <summary>
+        /// مسمط مثلث
+        /// </summary>
+        Mosammat3 = 32,
+
+        /// <summary>
+        /// مستزاد
+        /// </summary>
+        Mostazad = 40,
+
+        /// <summary>
+        /// رباعی مستزاد
+        /// </summary>
+        RobaeeMostazad = 45,
+
+        /// <summary>
+        /// مسمط مربع
+        /// </summary>
+        Mosammat4 = 64,
+
+        /// <summary>
+        /// مسمط مخمس
+        /// </summary>
+        Mosammat5 = 128,
+
+        /// <summary>
+        /// سپید
+        /// </summary>
+        Sepeed = 176,
+
+        /// <summary>
+        /// نیمایی یا سپید
+        /// </summary>
+        Now = 187,
+
+        /// <summary>
+        /// مسمط مسدس
+        /// </summary>
+        Mosammat6 = 256,
+
+        /// <summary>
+        /// مسمط مثمن
+        /// </summary>
+        Mosammat8 = 512,
+
+        /// <summary>
+        /// مسمط
+        /// </summary>
+        Mosammat = 992,
+
+        /// <summary>
+        /// چهارپاره
+        /// </summary>
+        ChaharPare = 1024,
+
+        /// <summary>
+        /// چند بندی
+        /// </summary>
+        MultiBand = 2032,
+
+    }
+}
