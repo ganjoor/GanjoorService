4fc0d0baf11a82dd4f3083ebae9b0dd2855d0033
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-04-23 14:36:46 +0430

    docs: #307 duplicating the search page for an independent similarity finder page

---
+++ GanjooRazor/Pages/Simi.cshtml.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using DNTPersianUtils.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RMuseum.Models.Ganjoor.ViewModels;
+using RMuseum.Services.Implementation;
+using RSecurityBackend.Models.Generic;
+
+
+namespace GanjooRazor.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class SimiModel : LoginPartialEnabledPageModel
+    {
+        /// <summary>
+        /// memory cache
+        /// </summary>
+        private readonly IMemoryCache _memoryCache;
+
+        /// <summary>
+        /// configration file reader (appsettings.json)
+        /// </summary>
+        private readonly IConfiguration Configuration;
+
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="httpClient"></param>
+        /// <param name="memoryCache"></param>
+        /// <param name="configuration"></param>
+        public SimiModel(HttpClient httpClient, IMemoryCache memoryCache, IConfiguration configuration) : base(httpClient)
+        {
+            _memoryCache = memoryCache;
+            Configuration
+                = configuration;
+        }
+
+        /// <summary>
+        /// aggressive cache
+        /// </summary>
+        public bool AggressiveCacheEnabled
+        {
+            get
+            {
+                try
+                {
+                    return bool.Parse(Configuration["AggressiveCacheEnabled"]);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public List<GanjoorPoetViewModel> Poets { get; set; }
+
+        public string Query { get; set; }
+        public int PoetId { get; set; }
+        public int CatId { get; set; }
+        public GanjoorPoetCompleteViewModel Poet { get; set; }
+        public List<GanjoorPoemCompleteViewModel> Poems { get; set; }
+        public string PagingToolsHtml { get; set; }
+
+        public string LastError { get; set; }
+
+        private async Task<bool> preparePoets()
+        {
+            var cacheKey = $"/api/ganjoor/poets";
+            if (!_memoryCache.TryGetValue(cacheKey, out List<GanjoorPoetViewModel> poets))
+            {
+                var response = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poets");
+                if (!response.IsSuccessStatusCode)
+                {
+                    LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                    return false;
+                }
+                poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+                if (AggressiveCacheEnabled)
+                {
+                    _memoryCache.Set(cacheKey, poets);
+                }
+            }
+
+            Poets = poets;
+            return true;
+        }
+
+        private async Task<bool> preparePoet()
+        {
+            var cacheKey = $"/api/ganjoor/poet/{PoetId}";
+            if (!_memoryCache.TryGetValue(cacheKey, out GanjoorPoetCompleteViewModel poet))
+            {
+                var poetResponse = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poet/{PoetId}");
+                if (!poetResponse.IsSuccessStatusCode)
+                {
+                    LastError = JsonConvert.DeserializeObject<string>(await poetResponse.Content.ReadAsStringAsync());
+                    return false;
+                }
+                poet = JObject.Parse(await poetResponse.Content.ReadAsStringAsync()).ToObject<GanjoorPoetCompleteViewModel>();
+                if (AggressiveCacheEnabled)
+                {
+                    _memoryCache.Set(cacheKey, poet);
+                }
+            }
+
+            Poet = poet;
+            return true;
+        }
+
+        public async Task<IActionResult> OnGetPoetInformationAsync(int id)
+        {
+            if (id == 0)
+                return new OkObjectResult(null);
+            var cacheKey = $"/api/ganjoor/poet/{id}";
+            if (!_memoryCache.TryGetValue(cacheKey, out GanjoorPoetCompleteViewModel poet))
+            {
+                var poetResponse = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poet/{id}");
+                if (!poetResponse.IsSuccessStatusCode)
+                {
+                    return BadRequest(JsonConvert.DeserializeObject<string>(await poetResponse.Content.ReadAsStringAsync()));
+                }
+                poet = JObject.Parse(await poetResponse.Content.ReadAsStringAsync()).ToObject<GanjoorPoetCompleteViewModel>();
+                if (AggressiveCacheEnabled)
+                {
+                    _memoryCache.Set(cacheKey, poet);
+                }
+            }
+            return new OkObjectResult(poet);
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            LoggedIn = !string.IsNullOrEmpty(Request.Cookies["Token"]);
+
+            Query = Request.Query["s"].ApplyCorrectYeKe().Trim();
+            bool quotes = Query.IndexOf("\"") != -1;
+            Query = LanguageUtils.MakeTextSearchable(Query); //replace zwnj with space
+            if (quotes)
+                Query = $"\"{Query}\"";
+            PoetId = string.IsNullOrEmpty(Request.Query["author"]) ? 0 : int.Parse(Request.Query["author"]);
+            CatId = string.IsNullOrEmpty(Request.Query["cat"]) ? 0 : int.Parse(Request.Query["cat"]);
+
+            ViewData["GoogleAnalyticsCode"] = Configuration["GoogleAnalyticsCode"];
+
+            //todo: use html master layout or make it partial
+            // 1. poets 
+            if (false == (await preparePoets()))
+                return Page();
+
+            var poetName = Poets.SingleOrDefault(p => p.Id == PoetId);
+            if (poetName != null)
+            {
+                ViewData["Title"] = $"گنجور » نتایج جستجو برای {Query} در آثار {poetName?.Name}";
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(Query))
+                {
+                    ViewData["Title"] = $"گنجور » نتایج جستجو برای {Query}";
+                }
+                else
+                {
+                    ViewData["Title"] = $"گنجور » جستجو";
+                }
+
+            }
+
+            if (PoetId != 0)
+            {
+                if (false == (await preparePoet()))
+                    return Page();
+            }
+
+            // 2. search verses
+            int pageNumber = 1;
+            if (!string.IsNullOrEmpty(Request.Query["page"]))
+            {
+                pageNumber = int.Parse(Request.Query["page"]);
+            }
+
+            HttpResponseMessage searchQueryResponse = null;
+
+            if (!string.IsNullOrEmpty(Query))
+            {
+                searchQueryResponse = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poems/search?term={Query}&poetId={PoetId}&catId={CatId}&PageNumber={pageNumber}&PageSize=20");
+
+                if (!searchQueryResponse.IsSuccessStatusCode)
+                {
+                    LastError = JsonConvert.DeserializeObject<string>(await searchQueryResponse.Content.ReadAsStringAsync());
+                    return Page();
+                }
+
+                Poems = JArray.Parse(await searchQueryResponse.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoemCompleteViewModel>>();
+            }
+
+
+            if (Poems != null && Poems.Count == 0)
+            {
+                Poems = null;
+            }
+
+            if (Poems != null)
+            {
+                // highlight searched word
+                string[] queryParts = Query.IndexOf('"') == 0 && Query.LastIndexOf('"') == (Query.Length - 1) ?
+                       new string[] { Query.Replace("\"", "") }
+                       :
+                       Query.Replace("\"", "").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var poem in Poems)
+                {
+                    string[] lines = poem.PlainText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+                    List<int> linesInExcerpt = new List<int>();
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        foreach (var queryPart in queryParts)
+                        {
+                            if (lines[i].IndexOf(queryPart) != -1)
+                            {
+                                if (i > 0)
+                                {
+                                    if (linesInExcerpt.IndexOf(i - 1) == -1)
+                                    {
+                                        linesInExcerpt.Add(i - 1);
+                                    }
+                                }
+                                if (linesInExcerpt.IndexOf(i) == -1)
+                                {
+                                    linesInExcerpt.Add(i);
+                                }
+
+                                if (i < (lines.Length - 1))
+                                    linesInExcerpt.Add(i + 1);
+
+                                break;
+                            }
+                        }
+                    }
+
+
+                    string plainText = "";
+                    for (int i = 0; i < linesInExcerpt.Count; i++)
+                    {
+                        if (linesInExcerpt[i] > 0 && linesInExcerpt.IndexOf(linesInExcerpt[i] - 1) == -1)
+                            plainText += "... ";
+                        plainText += $"{lines[linesInExcerpt[i]]}";
+                        if (linesInExcerpt[i] < (lines.Length - 1) && linesInExcerpt.IndexOf(linesInExcerpt[i] + 1) == -1)
+                            plainText += " ...";
+                        plainText += $"{Environment.NewLine}";
+                    }
+
+                    string finalPlainText = "";
+                    foreach (string line in plainText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        finalPlainText += $"<p>{line}</p>";
+                    }
+                    poem.PlainText = finalPlainText;
+
+                    for (int i = 0; i < queryParts.Length; i++)
+                    {
+                        string cssClass = i % 3 == 0 ? "hilite" : i % 3 == 1 ? "hilite2" : "hilite3";
+                        poem.PlainText = Regex.Replace(poem.PlainText, queryParts[i], $"<span class=\"{cssClass}\">{queryParts[i]}</span>", RegexOptions.IgnoreCase | RegexOptions.RightToLeft); ;
+                    }
+
+
+                }
+
+                if (searchQueryResponse != null)
+                {
+                    string paginationMetadataJsonValue = searchQueryResponse.Headers.GetValues("paging-headers").FirstOrDefault();
+
+                    if (!string.IsNullOrEmpty(paginationMetadataJsonValue))
+                    {
+                        PaginationMetadata paginationMetadata = JsonConvert.DeserializeObject<PaginationMetadata>(paginationMetadataJsonValue);
+                        string catQuery = "";
+                        if (!string.IsNullOrEmpty(Request.Query["cat"]))
+                        {
+                            catQuery = $"&cat={Request.Query["cat"]}";
+                        }
+                        PagingToolsHtml = GeneratePagingBarHtml(paginationMetadata, $"/search?s={WebUtility.UrlEncode(Query)}&amp;author={PoetId}{catQuery}");
+                    }
+                }
+
+
+            }
+
+
+
+            return Page();
+        }
+
+        private string GeneratePagingBarHtml(PaginationMetadata paginationMetadata, string routeStartWithQueryStrings)
+        {
+            string htmlText = "<p style=\"text-align: center;\">";
+
+
+            if (paginationMetadata != null && paginationMetadata.totalPages > 1)
+            {
+                if (paginationMetadata.currentPage > 3)
+                {
+                    htmlText += $"[<a href=\"{routeStartWithQueryStrings.Replace("\"", "%22")}&amp;page=1\">صفحهٔ اول</a>] …";
+                }
+                for (int i = (paginationMetadata.currentPage - 2); i <= (paginationMetadata.currentPage + 2); i++)
+                {
+                    if (i >= 1 && i <= paginationMetadata.totalPages)
+                    {
+                        htmlText += " [";
+                        if (i == paginationMetadata.currentPage)
+                        {
+                            htmlText += i.ToPersianNumbers();
+                        }
+                        else
+                        {
+                            htmlText += $"<a href=\"{routeStartWithQueryStrings.Replace("\"", "%22")}&amp;page={i}\">{i.ToPersianNumbers()}</a>";
+                        }
+                        htmlText += "] ";
+                    }
+                }
+                if (paginationMetadata.totalPages > (paginationMetadata.currentPage + 2))
+                {
+                    htmlText += $"… [<a href=\"{routeStartWithQueryStrings.Replace("\"", "%22")}&amp;page={paginationMetadata.totalPages}\">صفحهٔ آخر</a>]";
+                }
+            }
+
+            htmlText += $"</p>{Environment.NewLine}";
+            return htmlText;
+        }
+    }
+}
