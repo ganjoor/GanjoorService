b42245a759bd9d82e378f6d26e73753452f2eeed
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-05-23 13:02:34 +0430

    docs: #324 db migration

---
+++ RMuseum/Migrations/20220523083110_SectionInProbableMeters.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class SectionInProbableMeters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SectionId",
+                table: "GanjoorPoemProbableMetres",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SectionId",
+                table: "GanjoorPoemProbableMetres");
+        }
+    }
+}
