4f4d4d2e433dc8a629b334bed4b1c69175f5ae92
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-05-26 19:29:47 +0430

    docs: #327 db migration for edit/delete fields

---
+++ RMuseum/Migrations/20220526145910_CorrectionVPosDel.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class CorrectionVPosDel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MarkForDelete",
+                table: "GanjoorVerseVOrderText",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MarkForDeleteResult",
+                table: "GanjoorVerseVOrderText",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OriginalVersePosition",
+                table: "GanjoorVerseVOrderText",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VersePosition",
+                table: "GanjoorVerseVOrderText",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VersePositionResult",
+                table: "GanjoorVerseVOrderText",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarkForDelete",
+                table: "GanjoorVerseVOrderText");
+
+            migrationBuilder.DropColumn(
+                name: "MarkForDeleteResult",
+                table: "GanjoorVerseVOrderText");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalVersePosition",
+                table: "GanjoorVerseVOrderText");
+
+            migrationBuilder.DropColumn(
+                name: "VersePosition",
+                table: "GanjoorVerseVOrderText");
+
+            migrationBuilder.DropColumn(
+                name: "VersePositionResult",
+                table: "GanjoorVerseVOrderText");
+        }
+    }
+}
