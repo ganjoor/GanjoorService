98043cf885ad1a395539d3dacf87d572915c966f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-06-18 19:15:11 +0430

    docs: related sections not updating correctly bug fix

--- RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-ModerateSectionCorrection.cs
+++ RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-ModerateSectionCorrection.cs
@@ -81,7 +81,7 @@ namespace RMuseum.Services.Implementation
 
                         editingSectionNotTracked.Modified = editingSectionNotTracked.OldGanjoorMetreId != editingSectionNotTracked.GanjoorMetreId;
 
-                        foreach (var section in sections.Where(s => s.GanjoorMetreRefSectionIndex == editingSectionNotTracked.Index || s.Id == editingSectionNotTracked.Id))
+                        foreach (var section in sections.Where(s => s.GanjoorMetreRefSectionIndex == editingSectionNotTracked.Index))
                         {
                             section.GanjoorMetreId = editingSectionNotTracked.GanjoorMetreId;
                             section.Modified = editingSectionNotTracked.Modified;
