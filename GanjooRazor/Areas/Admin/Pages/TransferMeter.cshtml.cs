726578ff177b7c268b45ba6f693b049283c63dda
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-07-15 10:58:57 +0430

    docs: #347 UI for transfering meters

---
+++ GanjooRazor/Areas/Admin/Pages/TransferMeter.cshtml.cs
@@ -0,0 +1,99 @@
+﻿using GanjooRazor.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using RMuseum.Models.Ganjoor;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GanjooRazor.Areas.Admin.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class TransferMeterModel : PageModel
+    {
+        /// <summary>
+        /// rhythms alphabetically
+        /// </summary>
+        public GanjoorMetre[] RhythmsAlphabetically { get; set; }
+
+        /// <summary>
+        /// rhythms by frequency
+        /// </summary>
+        public GanjoorMetre[] RhythmsByVerseCount { get; set; }
+
+        /// <summary>
+        /// fatal error
+        /// </summary>
+        public string FatalError { get; set; }
+
+        /// <summary>
+        /// can edit
+        /// </summary>
+        public bool CanEdit { get; set; }
+
+        /// <summary>
+        /// get
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (string.IsNullOrEmpty(Request.Cookies["Token"]))
+                return Redirect("/");
+
+            FatalError = "";
+            CanEdit = Request.Cookies["CanEdit"] == "True";
+
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+
+                    var rhythmResponse = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/rhythms?sortOnVerseCount=true");
+                    if (!rhythmResponse.IsSuccessStatusCode)
+                    {
+                        FatalError = JsonConvert.DeserializeObject<string>(await rhythmResponse.Content.ReadAsStringAsync());
+                        return Page();
+                    }
+
+                    RhythmsByVerseCount = JsonConvert.DeserializeObject<GanjoorMetre[]>(await rhythmResponse.Content.ReadAsStringAsync());
+
+                    List<GanjoorMetre> rhythmsByVerseCount = new List<GanjoorMetre>(RhythmsByVerseCount);
+                    rhythmsByVerseCount.Sort((a, b) => a.Rhythm.CompareTo(b.Rhythm));
+                    rhythmsByVerseCount.Insert(0, new GanjoorMetre()
+                    {
+                        Rhythm = "null"
+                    }
+                    );
+                    rhythmsByVerseCount.Insert(0, new GanjoorMetre()
+                    {
+                        Rhythm = ""
+                    }
+                    );
+
+                    RhythmsAlphabetically = rhythmsByVerseCount.ToArray();
+
+                    var pageUrlResponse = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/pageurl?id={Request.Query["id"]}");
+                    if (!pageUrlResponse.IsSuccessStatusCode)
+                    {
+                        FatalError = JsonConvert.DeserializeObject<string>(await pageUrlResponse.Content.ReadAsStringAsync());
+                        return Page();
+                    }
+                    var pageUrl = JsonConvert.DeserializeObject<string>(await pageUrlResponse.Content.ReadAsStringAsync());
+
+                    var pageQuery = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/page?url={pageUrl}");
+                    if (!pageQuery.IsSuccessStatusCode)
+                    {
+                        FatalError = JsonConvert.DeserializeObject<string>(await pageQuery.Content.ReadAsStringAsync());
+                        return Page();
+                    }
+                }
+                else
+                {
+                    FatalError = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+            }
+            return Page();
+        }
+    }
+}
