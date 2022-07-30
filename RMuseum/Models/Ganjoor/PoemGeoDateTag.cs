f08eecebf950b5df4bd5a9b31b2fc74bdbd755d4
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-07-30 20:50:03 +0430

    docs: #352 adding PoemGeoDateTag model

---
+++ RMuseum/Models/Ganjoor/PoemGeoDateTag.cs
@@ -0,0 +1,58 @@
+﻿namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// Geo + date tags for poems
+    /// </summary>
+    public class PoemGeoDateTag
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Poem Id
+        /// </summary>
+        public int PoemId { get; set; }
+
+        /// <summary>
+        /// Poem
+        /// </summary>
+        public GanjoorPoem Poem { get; set; }
+
+        /// <summary>
+        /// couplet index
+        /// </summary>
+        public int CoupletIndex { get; set; }
+
+        /// <summary>
+        /// location
+        /// </summary>
+        public int? LocationId { get; set; }
+
+        /// <summary>
+        /// geo location
+        /// </summary>
+        public virtual GanjoorGeoLocation Location { get; set; }
+
+        /// <summary>
+        /// Hijri Ghamari year
+        /// </summary>
+        public int? LunarYear { get; set; }
+
+        /// <summary>
+        /// Hijri Ghamari month
+        /// </summary>
+        public int? LunarMonth { get; set; }
+
+        /// <summary>
+        /// Hijri Ghamari day
+        /// </summary>
+        public int? LunarDay{ get; set; }
+
+        /// <summary>
+        /// sample: 14440101, would be used in sorting events
+        /// </summary>
+        public int? LunarDateTotalNumber { get; set; }
+    }
+}
