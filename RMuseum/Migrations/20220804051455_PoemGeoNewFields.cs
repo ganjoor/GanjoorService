c2ddf265e500a6120afc4fe9ea1d9b3c07bc8b0c
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-08-04 09:46:55 +0430

    docs: #352 db migration

---
+++ RMuseum/Migrations/20220804051455_PoemGeoNewFields.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    public partial class PoemGeoNewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IgnoreInCategory",
+                table: "PoemGeoDateTags",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "VerifiedDate",
+                table: "PoemGeoDateTags",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IgnoreInCategory",
+                table: "PoemGeoDateTags");
+
+            migrationBuilder.DropColumn(
+                name: "VerifiedDate",
+                table: "PoemGeoDateTags");
+        }
+    }
+}
