d575e6f4bee81f55881b4a1ab0a87fa5bbf4dde3
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-02-11 21:54:10 +0330

    docs: #363 db migrations

---
+++ RMuseum/Migrations/20230211181406_PoemCorLang.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class PoemCorLang : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "GanjoorPoemCorrections",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LanguageReviewResult",
+                table: "GanjoorPoemCorrections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalLanguage",
+                table: "GanjoorPoemCorrections",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "GanjoorPoemCorrections");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageReviewResult",
+                table: "GanjoorPoemCorrections");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalLanguage",
+                table: "GanjoorPoemCorrections");
+        }
+    }
+}
