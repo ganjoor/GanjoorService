6e54221be599f91c3304857cf0df47dca1d4815d
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-02-11 21:11:22 +0330

    docs: #363 db migration

---
+++ RMuseum/Migrations/20230211173854_SecCorLang.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecCorLang : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "GanjoorPoemSectionCorrections",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LanguageReviewResult",
+                table: "GanjoorPoemSectionCorrections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalLanguage",
+                table: "GanjoorPoemSectionCorrections",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "GanjoorPoemSectionCorrections");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageReviewResult",
+                table: "GanjoorPoemSectionCorrections");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalLanguage",
+                table: "GanjoorPoemSectionCorrections");
+        }
+    }
+}
