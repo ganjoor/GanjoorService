7f7f4c995f5d78a01479d99820562ad635495d30
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-03-10 19:48:38 +0330

    docs: #369 db migration for Recitation.InitialScore

---
+++ RMuseum/Migrations/20230310161612_RecitationInitialScore.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecitationInitialScore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InitialScore",
+                table: "Recitations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitialScore",
+                table: "Recitations");
+        }
+    }
+}
