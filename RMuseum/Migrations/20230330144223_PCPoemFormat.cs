58696f23e2ac29b921b47a4e3bfcfd35718bc4e2
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-03-30 18:29:02 +0330

    docs: #373 db migration

---
+++ RMuseum/Migrations/20230330144223_PCPoemFormat.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class PCPoemFormat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OriginalPoemFormat",
+                table: "GanjoorPoemCorrections",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PoemFormat",
+                table: "GanjoorPoemCorrections",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PoemFormatReviewResult",
+                table: "GanjoorPoemCorrections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalPoemFormat",
+                table: "GanjoorPoemCorrections");
+
+            migrationBuilder.DropColumn(
+                name: "PoemFormat",
+                table: "GanjoorPoemCorrections");
+
+            migrationBuilder.DropColumn(
+                name: "PoemFormatReviewResult",
+                table: "GanjoorPoemCorrections");
+        }
+    }
+}
