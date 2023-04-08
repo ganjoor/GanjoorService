74984da9d8717ff1212016b42b750cc11fcbe36a
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-04-08 20:24:58 +0330

    docs: #379 #380 database migration

--- RMuseum/Migrations/20230408164943_CatFields.Designer.cs
+++ RMuseum/Migrations/20230408164943_CatFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RMuseum.DbContext;
 
@@ -11,9 +12,11 @@ using RMuseum.DbContext;
 namespace RMuseum.Migrations
 {
     [DbContext(typeof(RMuseumDbContext))]
-    partial class RMuseumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230408164943_CatFields")]
+    partial class CatFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RMuseum/Migrations/20230408164943_CatFields.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookName",
+                table: "GanjoorCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MapName",
+                table: "GanjoorCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RImageId",
+                table: "GanjoorCategories",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SumUpSubsGeoLocations",
+                table: "GanjoorCategories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GanjoorCategories_RImageId",
+                table: "GanjoorCategories",
+                column: "RImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GanjoorCategories_GeneralImages_RImageId",
+                table: "GanjoorCategories",
+                column: "RImageId",
+                principalTable: "GeneralImages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GanjoorCategories_GeneralImages_RImageId",
+                table: "GanjoorCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GanjoorCategories_RImageId",
+                table: "GanjoorCategories");
+
+            migrationBuilder.DropColumn(
+                name: "BookName",
+                table: "GanjoorCategories");
+
+            migrationBuilder.DropColumn(
+                name: "MapName",
+                table: "GanjoorCategories");
+
+            migrationBuilder.DropColumn(
+                name: "RImageId",
+                table: "GanjoorCategories");
+
+            migrationBuilder.DropColumn(
+                name: "SumUpSubsGeoLocations",
+                table: "GanjoorCategories");
+        }
+    }
+}
