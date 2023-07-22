f8b5d125c1701e13683139b851d096e31476154c
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-22 17:28:28 +0330

    #399 PDFLibraryController

---
+++ RMuseum/Controllers/PDFLibraryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RMuseum.Services;
+
+namespace RMuseum.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/pdf")]
+    public class PDFLibraryController : Controller
+    {
+        /// <summary>
+        /// PDF Service
+        /// </summary>
+        protected readonly IPDFLibraryService _pdfService;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="pdfService"></param>
+        public PDFLibraryController(IPDFLibraryService pdfService)
+        {
+            _pdfService = pdfService;
+        }
+    }
+}
