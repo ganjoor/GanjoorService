bcde6dfb51d983242a0a944ac254d0fb274dd828
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-22 11:41:50 +0330

    #398 database migration

---
+++ RMuseum/Migrations/20230722080604_PDFLibNewFields.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class PDFLibNewFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileMD5CheckSum",
+                table: "PDFBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalFileName",
+                table: "PDFBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StorageFolderName",
+                table: "PDFBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileMD5CheckSum",
+                table: "PDFBooks");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalFileName",
+                table: "PDFBooks");
+
+            migrationBuilder.DropColumn(
+                name: "StorageFolderName",
+                table: "PDFBooks");
+        }
+    }
+}
