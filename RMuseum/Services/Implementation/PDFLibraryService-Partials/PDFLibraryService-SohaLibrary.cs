dcf21afc01f768012a28499fe9c4c47adfc8a8df
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-25 18:40:13 +0330

    #401 refactoring
    Related Work Items: #4

---
+++ RMuseum/Services/Implementation/PDFLibraryService-Partials/PDFLibraryService-SohaLibrary.cs
@@ -0,0 +1,23 @@
+﻿using FluentFTP;
+using FluentFTP.Client.BaseClient;
+using Microsoft.EntityFrameworkCore;
+using RMuseum.DbContext;
+using RMuseum.Models.Artifact;
+using RMuseum.Models.ImportJob;
+using RMuseum.Models.PDFLibrary;
+using RSecurityBackend.Models.Generic;
+using RSecurityBackend.Models.Image;
+using RSecurityBackend.Services.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RMuseum.Services.Implementation
+{
+    public partial class PDFLibraryService
+    {
+    }
+}
