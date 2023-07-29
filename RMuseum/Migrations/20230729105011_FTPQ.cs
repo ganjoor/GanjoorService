3414eb878716af45c9735a10069345599ee38965
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-29 14:22:26 +0330

    #403 database migration

---
+++ RMuseum/Migrations/20230729105011_FTPQ.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class FTPQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QueuedFTPUploads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocalFilePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RemoteFilePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeleteFileAfterUpload = table.Column<bool>(type: "bit", nullable: false),
+                    QueueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Processing = table.Column<bool>(type: "bit", nullable: false),
+                    ProcessDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Error = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QueuedFTPUploads", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QueuedFTPUploads");
+        }
+    }
+}
