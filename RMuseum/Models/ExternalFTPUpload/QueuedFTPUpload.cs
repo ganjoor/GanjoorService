5a4f665a94b925a1ccee3796d64c68136765ae2c
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-07-29 10:44:18 +0330

    #403 QueuedFTPUpload model

---
+++ RMuseum/Models/ExternalFTPUpload/QueuedFTPUpload.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RMuseum.Models.ExternalFTPUpload
+{
+    /// <summary>
+    /// Uploadng Item
+    /// </summary>
+    public class QueuedFTPUpload
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// local file path
+        /// </summary>
+        public string LocalFilePath { get; set; }
+
+        /// <summary>
+        /// remote file path
+        /// </summary>
+        public string RemoteFilePath { get; set; }
+
+        /// <summary>
+        /// delete file after upload
+        /// </summary>
+        public bool DeleteFileAfterUpload { get; set; }
+
+        /// <summary>
+        /// queue date
+        /// </summary>
+        public DateTime QueueDate { get; set; }
+
+        /// <summary>
+        /// processing
+        /// </summary>
+        public bool Processing { get; set; }
+
+        /// <summary>
+        /// process date
+        /// </summary>
+        public DateTime? ProcessDate { get; set; }
+
+        /// <summary>
+        /// error
+        /// </summary>
+        public string Error { get; set; }
+    }
+}
