e327f093f4096b0587389fd2c5d60a254d04a462
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-08-11 12:03:03 +0330

    #407 db migration

---
+++ RMuseum/Migrations/20230811083105_PDFBookText.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class PDFBookText : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookText",
+                table: "PDFBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookText",
+                table: "PDFBooks");
+        }
+    }
+}
