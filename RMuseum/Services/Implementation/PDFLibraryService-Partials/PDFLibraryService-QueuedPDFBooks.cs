e4bbaff175f87b14551d0cc9116b7b9ff1e8011a
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-01 11:10:54 +0330

    #409 PDFLibraryService-QueuedPDFBooks.cs

---
+++ RMuseum/Services/Implementation/PDFLibraryService-Partials/PDFLibraryService-QueuedPDFBooks.cs
@@ -0,0 +1,23 @@
+﻿using ganjoor;
+using Microsoft.EntityFrameworkCore;
+using RMuseum.DbContext;
+using RMuseum.Models.Artifact;
+using RMuseum.Models.ImportJob;
+using RMuseum.Models.PDFLibrary;
+using RMuseum.Models.PDFLibrary.ViewModels;
+using RSecurityBackend.Models.Generic;
+using RSecurityBackend.Models.Image;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace RMuseum.Services.Implementation
+{
+    public partial class PDFLibraryService
+    {
+
+    }
+}
