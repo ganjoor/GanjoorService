6275bc855e72d54196ef5761ad15ebe697e623fd
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-01 14:19:34 +0330

    #409 database migration

---
+++ RMuseum/Migrations/20230901104421_DnQ.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class DnQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QueuedPDFBooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DownloadOrder = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AuthorsLine = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsTranslation = table.Column<bool>(type: "bit", nullable: false),
+                    TranslatorsLine = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OriginalSourceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OriginalSourceUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OriginalFileUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Processed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QueuedPDFBooks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QueuedPDFBooks");
+        }
+    }
+}
