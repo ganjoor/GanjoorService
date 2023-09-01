a0056a7b86d0d320e7d1726a295d2f9ae35bdd10
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-01 10:27:50 +0330

    #409 QueuedPDFBook model

---
+++ RMuseum/Models/PDFLibrary/QueuedPDFBook.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RMuseum.Models.PDFLibrary
+{
+    /// <summary>
+    /// a fake PDFBook for scheduling downloads
+    /// </summary>
+    public class QueuedPDFBook
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Download Order
+        /// </summary>
+        public int DownloadOrder { get; set; }
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// authors (descriptive)
+        /// </summary>
+        public string AuthorsLine { get; set; }
+
+        /// <summary>
+        /// description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// language
+        /// </summary>
+        public string Language { get; set; }
+
+        /// <summary>
+        /// is translation
+        /// </summary>
+        public bool IsTranslation { get; set; }
+
+        /// <summary>
+        /// translators (descriptive)
+        /// </summary>
+        public string TranslatorsLine { get; set; }
+
+        /// <summary>
+        /// original source name
+        /// </summary>
+        public string OriginalSourceName { get; set; }
+
+        /// <summary>
+        /// original source url
+        /// </summary>
+        public string OriginalSourceUrl { get; set; }
+
+        /// <summary>
+        /// original file url
+        /// </summary>
+        public string OriginalFileUrl { get; set; }
+    }
+}
