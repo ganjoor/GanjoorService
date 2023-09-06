74fdbee3b01e54043db49ac8afb47fb51224885d
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-06 10:40:28 +0330

    #409 database migration

---
+++ RMuseum/Migrations/20230906070807_QProcessResult.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class QProcessResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessResult",
+                table: "QueuedPDFBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessResult",
+                table: "QueuedPDFBooks");
+        }
+    }
+}
