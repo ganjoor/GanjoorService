6da7c22afe5365f8a3636d983d4f291035e92d4b
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-06 13:53:37 +0330

    #409 database migration

---
+++ RMuseum/Migrations/20230906101704_QResultId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class QResultId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResultId",
+                table: "QueuedPDFBooks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResultId",
+                table: "QueuedPDFBooks");
+        }
+    }
+}
