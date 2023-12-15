e09c2b5deecf97e720bbc47ea866b08e0542e921
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-12-15 14:04:50 +0330

    #418 database migration

---
+++ RMuseum/Migrations/20231215103132_QPSameField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class QPSameField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SamePoemsQuotedCount",
+                table: "GanjoorQuotedPoems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SamePoemsQuotedCount",
+                table: "GanjoorQuotedPoems");
+        }
+    }
+}
