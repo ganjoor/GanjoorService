3199991f6c5df5f352ce91bd2543cd471c75b702
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-12-21 10:02:28 +0330

    #418 SuggestQuoted page

---
+++ GanjooRazor/Areas/Admin/Pages/SuggestQuoted.cshtml.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RMuseum.Models.Ganjoor.ViewModels;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using RMuseum.Models.Ganjoor;
+
+namespace GanjooRazor.Areas.Admin.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class SuggestQuotedModel : PageModel
+    {
+        public string LastMessage { get; set; }
+
+        /// <summary>
+        /// HttpClient instance
+        /// </summary>
+        private readonly HttpClient _httpClient;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="httpClient"></param>
+        public SuggestQuotedModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public GanjoorPoemCompleteViewModel Poem { get; set; }
+
+        public Tuple<int, string>[] Couplets { get; set; }
+
+        [BindProperty]
+        public GanjoorQuotedPoem GanjoorQuotedPoem { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (string.IsNullOrEmpty(Request.Cookies["Token"]))
+                return Redirect("/");
+
+
+            if (string.IsNullOrEmpty(Request.Query["p"]))
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "شناسهٔ شعر مشخص نیست.");
+            }
+
+            int poemId = int.Parse(Request.Query["p"]);
+
+            GanjoorQuotedPoem  = new GanjoorQuotedPoem()
+            {
+                PoemId = poemId,
+               
+            };
+
+            var pageUrlResponse = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/pageurl?id={poemId}");
+            if (!pageUrlResponse.IsSuccessStatusCode)
+            {
+                LastMessage = JsonConvert.DeserializeObject<string>(await pageUrlResponse.Content.ReadAsStringAsync());
+                return Page();
+            }
+
+            var pageUrl = JsonConvert.DeserializeObject<string>(await pageUrlResponse.Content.ReadAsStringAsync());
+
+            var pageQuery = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/page?url={pageUrl}");
+            if (!pageQuery.IsSuccessStatusCode)
+            {
+                LastMessage = JsonConvert.DeserializeObject<string>(await pageQuery.Content.ReadAsStringAsync());
+                return Page();
+            }
+            var pageInformation = JObject.Parse(await pageQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPageCompleteViewModel>();
+
+            Poem = pageInformation.Poem;
+
+            int coupetIndex = -1;
+            string coupletText = "";
+            List<Tuple<int, string>> couplets = [new Tuple<int, string>(-1, "*")];
+            int verseIndex = 0;
+            bool incompleteCouplet = false;
+            while (verseIndex < pageInformation.Poem.Verses.Length)
+            {
+
+                switch (pageInformation.Poem.Verses[verseIndex].VersePosition)
+                {
+                    case VersePosition.Comment:
+                        incompleteCouplet = false;
+                        if (!string.IsNullOrEmpty(coupletText))
+                        {
+                            couplets.Add(new Tuple<int, string>(coupetIndex, coupletText));
+                            coupletText = "";
+                        }
+                        break;
+                    case VersePosition.Paragraph:
+                    case VersePosition.Single:
+                        incompleteCouplet = false;
+                        if (!string.IsNullOrEmpty(coupletText))
+                        {
+                            couplets.Add(new Tuple<int, string>(coupetIndex, coupletText));
+                            coupletText = "";
+                        }
+                        coupetIndex++;
+                        couplets.Add(new Tuple<int, string>(coupetIndex, pageInformation.Poem.Verses[verseIndex].Text));
+                        break;
+                    case VersePosition.Right:
+                    case VersePosition.CenteredVerse1:
+                        incompleteCouplet = false;
+                        if (!string.IsNullOrEmpty(coupletText))
+                        {
+                            couplets.Add(new Tuple<int, string>(coupetIndex, coupletText));
+                        }
+                        coupetIndex++;
+                        coupletText = pageInformation.Poem.Verses[verseIndex].Text;
+                        break;
+                    case VersePosition.Left:
+                    case VersePosition.CenteredVerse2:
+                        incompleteCouplet = true;
+                        coupletText += $" {pageInformation.Poem.Verses[verseIndex].Text}";
+                        break;
+                }
+                verseIndex++;
+            }
+
+
+            if (incompleteCouplet && !string.IsNullOrEmpty(coupletText))
+                couplets.Add(new Tuple<int, string>(coupetIndex, coupletText));
+
+            Couplets = couplets.ToArray();
+
+            return Page();
+        }
+    }
+}
