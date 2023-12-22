47ac2b2fc0172aa979b48075e1d303378dcc2be8
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-12-22 18:08:23 +0330

    #418 DiscoverQuotedQueueItem model

---
+++ RMuseum/Models/Ganjoor/DiscoverQuotedQueueItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    /// <summary>
+    /// discover quoted q item
+    /// </summary>
+    public class DiscoverQuotedQueueItem
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// poet id
+        /// </summary>
+        public int PoetId { get; set; }
+
+        /// <summary>
+        /// poem id
+        /// </summary>
+        public int PoemId { get; set; }
+
+        /// <summary>
+        /// related poet id
+        /// </summary>
+        public int RelatedPoetId { get; set; }
+
+        /// <summary>
+        /// related poem id
+        /// </summary>
+        public int RelatedPoemId { get; set; }
+    }
+}
