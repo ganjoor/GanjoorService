aa31f79ab256a50ead72fcaa3e4648dd9e9169fd
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-12-22 19:13:17 +0330

    #418 discover db migration

---
+++ RMuseum/Migrations/20231222153950_DisQ.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiscoverQuotedQueueItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PoetId = table.Column<int>(type: "int", nullable: false),
+                    PoemId = table.Column<int>(type: "int", nullable: false),
+                    RelatedPoetId = table.Column<int>(type: "int", nullable: false),
+                    RelatedPoemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscoverQuotedQueueItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscoverQuotedQueueItems");
+        }
+    }
+}
