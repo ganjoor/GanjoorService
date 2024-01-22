edf96876fdfd8f823d235b84a84eb908efaf26a9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-01-22 21:51:50 +0330

    #422 fix

--- RMuseum/Services/Implementation/LanguageUtils.cs
+++ RMuseum/Services/Implementation/LanguageUtils.cs
@@ -79,7 +79,6 @@ namespace RMuseum.Services.Implementation
         public static string SameSoundLetters(string text)
         {
             return text.Replace("ث", "س").Replace("ح", "ه").Replace("ذ", "ز").Replace("ص", "س").Replace("ض", "ز").Replace("ط", "ت").Replace("ظ", "ز").Replace("غ", "ق");
-                       ;
         }
 
         private static string PrepareTextForFindingRhyme(string text)
