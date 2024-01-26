fec424ae5668e7626c21df7b780a6b09cb37fb23
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-01-26 14:03:21 +0330

    #424 poet quotes page
    Related Work Items: #4

--- GanjooRazor/Pages/Quotes.cshtml.cs
+++ GanjooRazor/Pages/Quotes.cshtml.cs
@@ -17,9 +17,9 @@ namespace GanjooRazor.Pages
             Configuration = configuration;
         }
         public string LastError { get; set; }
-
         public List<GanjoorPoetViewModel> Poets { get; set; }
         public GanjoorPoetViewModel Poet { get; set; }
+        public List<GanjoorQuotedPoemViewModel> ClaimedQuotes { get; set; }
         public List<GanjoorQuotedPoemViewModel> Quotes { get; set; }
 
         private async Task<bool> preparePoets()
@@ -66,7 +66,7 @@ namespace GanjooRazor.Pages
             {
                 url += $"&poetId={Poet.Id}";
             }
-            var responseQuotes = await _httpClient.GetAsync(url);
+            var responseQuotes = await _httpClient.GetAsync(url + "&claimed=false");
             if (!responseQuotes.IsSuccessStatusCode)
             {
                 LastError = JsonConvert.DeserializeObject<string>(await responseQuotes.Content.ReadAsStringAsync());
@@ -76,6 +76,32 @@ namespace GanjooRazor.Pages
             Quotes = JArray.Parse(await responseQuotes.Content.ReadAsStringAsync()).ToObject<List<GanjoorQuotedPoemViewModel>>();
 
             foreach (var quote in Quotes)
+            {
+                var poemQuery = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poem/{quote.PoemId}");
+                if (!poemQuery.IsSuccessStatusCode)
+                {
+                    LastError = JsonConvert.DeserializeObject<string>(await poemQuery.Content.ReadAsStringAsync());
+                    return Page();
+                }
+                var poem = JObject.Parse(await poemQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPoemCompleteViewModel>();
+                quote.Poem = new RMuseum.Models.Ganjoor.GanjoorPoem()
+                {
+                    Id = poem.Id,
+                    FullTitle = poem.FullTitle,
+                    FullUrl = poem.FullUrl,
+                };
+            }
+
+            var responseClaimedQuotes = await _httpClient.GetAsync(url + "&claimed=true");
+            if (!responseClaimedQuotes.IsSuccessStatusCode)
+            {
+                LastError = JsonConvert.DeserializeObject<string>(await responseClaimedQuotes.Content.ReadAsStringAsync());
+                return Page();
+            }
+
+            ClaimedQuotes = JArray.Parse(await responseClaimedQuotes.Content.ReadAsStringAsync()).ToObject<List<GanjoorQuotedPoemViewModel>>();
+
+            foreach (var quote in ClaimedQuotes)
             {
                 var poemQuery = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poem/{quote.PoemId}");
                 if (!poemQuery.IsSuccessStatusCode)
