2bb4265c51b2d066dab0cf9f8b04d31058760b7f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-01-26 13:10:51 +0330

    #424 poet quotes page

---
+++ GanjooRazor/Pages/Quotes.cshtml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RMuseum.Models.Ganjoor.ViewModels;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GanjooRazor.Pages
+{
+    public class QuotesModel : LoginPartialEnabledPageModel
+    {
+        private readonly IConfiguration Configuration;
+        public QuotesModel(HttpClient httpClient, IConfiguration configuration) : base(httpClient)
+        {
+            Configuration = configuration;
+        }
+        public string LastError { get; set; }
+
+        public List<GanjoorPoetViewModel> Poets { get; set; }
+        public GanjoorPoetViewModel Poet { get; set; }
+        public List<GanjoorQuotedPoemViewModel> Quotes { get; set; }
+
+        private async Task<bool> preparePoets()
+        {
+            var response = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poets");
+            if (!response.IsSuccessStatusCode)
+            {
+                LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                return false;
+            }
+            Poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+
+            return true;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (bool.Parse(Configuration["MaintenanceMode"]))
+            {
+                return StatusCode(503);
+            }
+
+            LoggedIn = !string.IsNullOrEmpty(Request.Cookies["Token"]);
+            ViewData["GoogleAnalyticsCode"] = Configuration["GoogleAnalyticsCode"];
+
+            //todo: use html master layout or make it partial
+            if (false == (await preparePoets()))
+                return Page();
+
+            if (!string.IsNullOrEmpty(Request.Query["p"]))
+            {
+                var response = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poet?url=/{Request.Query["p"]}");
+                if (!response.IsSuccessStatusCode)
+                {
+                    LastError = JsonConvert.DeserializeObject<string>(await response.Content.ReadAsStringAsync());
+                    return Page();
+                }
+                Poet = JObject.Parse(await response.Content.ReadAsStringAsync()).ToObject<GanjoorPoetCompleteViewModel>().Poet;
+                Poet.ImageUrl = $"{APIRoot.InternetUrl}{Poet.ImageUrl}";
+            }
+
+
+            var url = $"{APIRoot.Url}/api/ganjoor/quoted?published=true";
+            if (Poet != null)
+            {
+                url += $"&poetId={Poet.Id}";
+            }
+            var responseQuotes = await _httpClient.GetAsync(url);
+            if (!responseQuotes.IsSuccessStatusCode)
+            {
+                LastError = JsonConvert.DeserializeObject<string>(await responseQuotes.Content.ReadAsStringAsync());
+                return Page();
+            }
+
+            Quotes = JArray.Parse(await responseQuotes.Content.ReadAsStringAsync()).ToObject<List<GanjoorQuotedPoemViewModel>>();
+
+            foreach (var quote in Quotes)
+            {
+                var poemQuery = await _httpClient.GetAsync($"{APIRoot.Url}/api/ganjoor/poem/{quote.PoemId}");
+                if (!poemQuery.IsSuccessStatusCode)
+                {
+                    LastError = JsonConvert.DeserializeObject<string>(await poemQuery.Content.ReadAsStringAsync());
+                    return Page();
+                }
+                var poem = JObject.Parse(await poemQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPoemCompleteViewModel>();
+                quote.Poem = new RMuseum.Models.Ganjoor.GanjoorPoem()
+                {
+                    Id = poem.Id,
+                    FullTitle = poem.FullTitle,
+                    FullUrl = poem.FullUrl,
+                };
+            }
+
+            
+
+
+            ViewData["Title"] = Poet == null ? "نقل قول‌های شاعران" : $"نقل‌قول‌ها و شعرهای مرتبط {Poet.Nickname}";
+
+            return Page();
+        }
+    }
+}
