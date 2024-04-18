58d44b10b321be743db42b45b13f863a25569c7c
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-04-18 17:45:52 +0330

    database migration

---
+++ RMuseum/Migrations/20240418141214_NotifTypeLongjUpdate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotifTypeLongjUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NotificationType",
+                table: "Notifications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdate",
+                table: "LongRunningJobs",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotificationType",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdate",
+                table: "LongRunningJobs");
+        }
+    }
+}
