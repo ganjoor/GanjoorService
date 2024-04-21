6a8350b7575079a4d84ab27ad85a3445a08a78b5
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-04-21 16:29:00 +0330

    #435 database migration

---
+++ RMuseum/Migrations/20240421125807_MatchPer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchPer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MatchPercent",
+                table: "PinterestLinks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MatchPercent",
+                table: "PinterestLinks");
+        }
+    }
+}
