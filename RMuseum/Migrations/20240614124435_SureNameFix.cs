6aafd1979158fe35e4e08d117a4cf4296bea6a3c
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-14 16:15:37 +0330

    SureName => SurName: db migration

---
+++ RMuseum/Migrations/20240614124435_SureNameFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class SureNameFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SureName",
+                table: "AspNetUsers",
+                newName: "SurName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SurName",
+                table: "AspNetUsers",
+                newName: "SureName");
+        }
+    }
+}
