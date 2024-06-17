a8874a2a16a5e8fe365eb8b1f133ffeba7e3cba3
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-17 11:03:42 +0330

    #422 DigitalSource structure

---
+++ RMuseum/Models/Ganjoor/DigitalSource.cs
@@ -0,0 +1,30 @@
+﻿namespace RMuseum.Models.Ganjoor
+{
+    public class DigitalSource
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// source url slug
+        /// </summary>
+        public string UrlSlug { get; set; }
+
+        /// <summary>
+        /// short name
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// full name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// source category
+        /// </summary>
+        public string SourceCategory { get; set; }
+    }
+}
