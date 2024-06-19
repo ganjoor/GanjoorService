42d7f1f3f0864fb9d7a4c00701b8a525bbe11ea4
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-19 22:03:48 +0330

    #444 GanjoorService-CategoryWordCounts

---
+++ RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-CategoryWordCounts.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RMuseum.Models.Ganjoor;
+using System;
+using System.Data;
+using System.Linq;
+using RMuseum.DbContext;
+using System.Collections.Generic;
+using RSecurityBackend.Services.Implementation;
+
+namespace RMuseum.Services.Implementation
+{
+    /// <summary>
+    /// IGanjoorService implementation
+    /// </summary>
+    public partial class GanjoorService : IGanjoorService
+    {
+        public void BuildCategoryWordCounts()
+        {
+            _backgroundTaskQueue.QueueBackgroundWorkItem
+                          (
+                          async token =>
+                          {
+                              using (RMuseumDbContext context = new RMuseumDbContext(new DbContextOptions<RMuseumDbContext>())) //this is long running job, so _context might be already been freed/collected by GC
+                              {
+                                  LongRunningJobProgressServiceEF jobProgressServiceEF = new LongRunningJobProgressServiceEF(context);
+                                  var job = (await jobProgressServiceEF.NewJob($"BuildCategoryWordCounts", "Query data")).Result;
+                                  try
+                                  {
+                                      await jobProgressServiceEF.UpdateJob(job.Id, 100);
+                                  }
+                                  catch (Exception exp)
+                                  {
+                                      await jobProgressServiceEF.UpdateJob(job.Id, 100, "", false, exp.ToString());
+                                  }
+
+                              }
+                          });
+        }
+    }
+}
