bb351ddd2af313063f1a481ceb5c0c9fcc15fdaf
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-19 21:29:21 +0330

    #444 CategoryWordCount

---
+++ RMuseum/Models/Ganjoor/CategoryWordCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    public class CategoryWordCount
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// CatId
+        /// </summary>
+        public int CatId { get; set; }
+
+        /// <summary>
+        /// Word
+        /// </summary>
+        public string Word { get; set; }
+
+        /// <summary>
+        /// Count
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// is root cat
+        /// </summary>
+        public bool PoetCat { get; set; }
+    }
+}
