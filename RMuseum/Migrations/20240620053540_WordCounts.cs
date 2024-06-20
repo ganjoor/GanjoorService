43fae4180ea2df537a11d9cfc6819d37bd5b6a28
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-20 09:07:39 +0330

    #444 db migration

---
+++ RMuseum/Migrations/20240620053540_WordCounts.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class WordCounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryWordCounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CatId = table.Column<int>(type: "int", nullable: false),
+                    Word = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    PoetCat = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryWordCounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryWordCounts_CatId_Word",
+                table: "CategoryWordCounts",
+                columns: new[] { "CatId", "Word" },
+                unique: true,
+                filter: "[Word] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryWordCounts");
+        }
+    }
+}
