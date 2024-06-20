78a504298d39c00f2e2256ae2dce12e55b16fb44
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-20 08:45:15 +0330

    _BuildCategoryWordStatsAsync

--- RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-CategoryWordCounts.cs
+++ RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService-CategoryWordCounts.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using RMuseum.DbContext;
 using System.Collections.Generic;
 using RSecurityBackend.Services.Implementation;
+using System.Threading.Tasks;
 
 namespace RMuseum.Services.Implementation
 {
@@ -26,6 +27,12 @@ namespace RMuseum.Services.Implementation
                                   var job = (await jobProgressServiceEF.NewJob($"BuildCategoryWordCounts", "Query data")).Result;
                                   try
                                   {
+                                      var poets = await context.GanjoorPoets.AsNoTracking().Where(p => p.Published).OrderBy(p => p.Id).ToListAsync();
+                                      foreach (var poet in poets)
+                                      {
+                                          var poetCat = await context.GanjoorCategories.AsNoTracking().Where(c => c.PoetId == poet.Id && c.ParentId == null).SingleAsync();
+                                          await _BuildCategoryWordStatsAsync(context, poetCat);
+                                      }
                                       await jobProgressServiceEF.UpdateJob(job.Id, 100);
                                   }
                                   catch (Exception exp)
@@ -36,5 +43,55 @@ namespace RMuseum.Services.Implementation
                               }
                           });
         }
+
+        private async Task<List<CategoryWordCount>> _BuildCategoryWordStatsAsync(RMuseumDbContext context, GanjoorCat cat)
+        {
+            List<CategoryWordCount> counts = new List<CategoryWordCount>();
+            var children = await context.GanjoorCategories.AsNoTracking().Where(c => c.ParentId == cat.Id).ToListAsync();
+            foreach (var child in children)
+            {
+                var subcatCounts = await _BuildCategoryWordStatsAsync(context, child);
+                if (subcatCounts.Any())
+                {
+                    foreach (var count in subcatCounts)
+                    {
+                        var wordCount = counts.Where(c => c.CatId == cat.Id && c.Word == count.Word).SingleOrDefault();
+                        if (wordCount != null)
+                        {
+                            wordCount.Count += count.Count;
+                        }
+                        else
+                        {
+                            counts.Add(new CategoryWordCount { CatId = cat.Id, Word = count.Word, Count = count.Count });
+                        }
+                    }
+                    counts.AddRange(subcatCounts);
+                }
+            }
+
+            var poems = await context.GanjoorPoems.AsNoTracking().Where(p => p.CatId == cat.Id).ToListAsync();
+            foreach (var poem in poems)
+            {
+                var verses = await context.GanjoorVerses.AsNoTracking().Where(v => v.PoemId == poem.Id && v.VersePosition != VersePosition.Comment).OrderBy(v => v.VOrder).ToListAsync();
+                foreach (var verse in verses)
+                {
+                    string[] words = verse.Text.Split([' ', '‌']);
+                    foreach (var word in words)
+                    {
+                        var wordCount = counts.Where(c => c.CatId == cat.Id && c.Word == word).SingleOrDefault();
+                        if (wordCount != null)
+                        {
+                            wordCount.Count++;
+                        }
+                        else
+                        {
+                            counts.Add(new CategoryWordCount { CatId = cat.Id, Word = word, Count = 1 });
+                        }
+                    }
+                }
+            }
+            return counts;
+        }
+
     }
 }
