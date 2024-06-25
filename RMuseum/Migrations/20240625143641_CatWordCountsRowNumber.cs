c18eec5e8ca4f0b74e48bad20410622ca9d54258
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-25 18:38:10 +0330

    #444 db migration

---
+++ RMuseum/Migrations/20240625143641_CatWordCountsRowNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatWordCountsRowNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RowNmbrInCat",
+                table: "CategoryWordCounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowNmbrInCat",
+                table: "CategoryWordCounts");
+        }
+    }
+}
