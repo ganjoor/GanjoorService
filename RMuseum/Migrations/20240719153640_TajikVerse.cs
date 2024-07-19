a9fc47d001da33481ee269bc1503feab8446c68d
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-07-19 19:07:18 +0330

    #445 db migration : GanjoorVerse + Tajik

---
+++ RMuseum/Migrations/20240719153640_TajikVerse.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class TajikVerse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tajik",
+                table: "GanjoorVerses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tajik",
+                table: "GanjoorVerses");
+        }
+    }
+}
