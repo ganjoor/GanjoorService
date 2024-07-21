8b2e11007722087edf4f06a6bc7758623aad174c
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-07-21 10:03:15 +0330

    GetTajikPoetsAsync

---
+++ RMuseum/Services/Implementation/GanjoorService-Partials/GanjoorService_Tajik.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using RMuseum.Models.Ganjoor;
+using System.Threading.Tasks;
+using RSecurityBackend.Models.Generic;
+
+namespace RMuseum.Services.Implementation
+{
+    /// <summary>
+    /// IGanjoorService implementation
+    /// </summary>
+    public partial class GanjoorService : IGanjoorService
+    {
+        /// <summary>
+        /// tajik poets
+        /// </summary>
+        /// <returns></returns>
+        public async Task<RServiceResult<GanjoorTajikPoet[]>> GetTajikPoetsAsync()
+        {
+            try
+            {
+                return new RServiceResult<GanjoorTajikPoet[]>(await _context.TajikPoets.AsNoTracking().ToArrayAsync());
+            }
+            catch (Exception exp)
+            {
+                return new RServiceResult<GanjoorTajikPoet[]>(null, exp.ToString());
+            }
+        }
+    }
+}
