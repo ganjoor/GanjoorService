e6ebc8e8784d11698cb9a36d29c17315c15dc3e0
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-12-12 17:42:03 +0330

    #454 db migrations

---
+++ RMuseum/Migrations/20241212141055_GeoAIFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class GeoAIFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MachineGenerated",
+                table: "PoemGeoDateTags",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MachineGenerated",
+                table: "GanjoorRelatedPersons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MachineGenerated",
+                table: "GanjoorGeoLocations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineGenerated",
+                table: "PoemGeoDateTags");
+
+            migrationBuilder.DropColumn(
+                name: "MachineGenerated",
+                table: "GanjoorRelatedPersons");
+
+            migrationBuilder.DropColumn(
+                name: "MachineGenerated",
+                table: "GanjoorGeoLocations");
+        }
+    }
+}
