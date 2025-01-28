9fc89db467c2be0e033e2b9f790a2574c50d3d9e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-01-28 13:34:28 +0330

    stats for poems couplet counts in categories #464

---
+++ RMuseum/Migrations/20250128100350_SectionCoupletCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class SectionCoupletCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CoupletsCount",
+                table: "GanjoorPoemSections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoupletsCount",
+                table: "GanjoorPoemSections");
+        }
+    }
+}
