c6916743a373e1d2d227ba05a91f73c94730cb6e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-03-23 10:08:48 +0330

    #467 ReviewCatEdits

---
+++ GanjooRazor/Areas/Admin/Pages/ReviewCatEdits.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using GanjooRazor.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RMuseum.Models.Ganjoor.ViewModels;
+using RSecurityBackend.Models.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace GanjooRazor.Areas.Admin.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class ReviewCatEditsModel : PageModel
+    {
+        /// <summary>
+        /// correction
+        /// </summary>
+        public GanjoorCatCorrectionViewModel Correction { get; set; }
+        /// <summary>
+        /// fatal error
+        /// </summary>
+        public string FatalError { get; set; }
+
+        /// <summary>
+        /// skip
+        /// </summary>
+        public int Skip { get; set; }
+
+        /// <summary>
+        /// total count
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// page
+        /// </summary>
+        public GanjoorPageCompleteViewModel PageInformation { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (string.IsNullOrEmpty(Request.Cookies["Token"]))
+                return Redirect("/");
+
+            FatalError = "";
+            TotalCount = 0;
+            Skip = string.IsNullOrEmpty(Request.Query["skip"]) ? 0 : int.Parse(Request.Query["skip"]);
+            using (HttpClient secureClient = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(secureClient, Request, Response))
+                {
+                    var nextResponse = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/cat/correction/next?skip={Skip}");
+                    if (!nextResponse.IsSuccessStatusCode)
+                    {
+                        FatalError = JsonConvert.DeserializeObject<string>(await nextResponse.Content.ReadAsStringAsync());
+                        return Page();
+                    }
+
+                    string paginnationMetadata = nextResponse.Headers.GetValues("paging-headers").FirstOrDefault();
+                    if (!string.IsNullOrEmpty(paginnationMetadata))
+                    {
+                        TotalCount = JsonConvert.DeserializeObject<PaginationMetadata>(paginnationMetadata).totalCount;
+                    }
+                    Correction = JsonConvert.DeserializeObject<GanjoorCatCorrectionViewModel>(await nextResponse.Content.ReadAsStringAsync());
+                    if (Correction != null)
+                    {
+                        var catQuery = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/cat/{Correction.CatId}");
+                        if (!catQuery.IsSuccessStatusCode)
+                        {
+                            FatalError = JsonConvert.DeserializeObject<string>(await catQuery.Content.ReadAsStringAsync());
+                            return Page();
+                        }
+                        var cat = JsonConvert.DeserializeObject<GanjoorPoetCompleteViewModel>(await catQuery.Content.ReadAsStringAsync());
+                        var pageQuery = await secureClient.GetAsync($"{APIRoot.Url}/api/ganjoor/page?url={cat.Cat.FullUrl}");
+                        if (!pageQuery.IsSuccessStatusCode)
+                        {
+                            FatalError = JsonConvert.DeserializeObject<string>(await pageQuery.Content.ReadAsStringAsync());
+                            return Page();
+                        }
+                        PageInformation = JObject.Parse(await pageQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPageCompleteViewModel>();
+
+                    }
+
+                }
+                else
+                {
+                    FatalError = "لطفاً از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+            }
+            return Page();
+        }
+    }
+}
