232b5eae5ca087cfd19bd69cf70dd2b03692bdba
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-03-26 14:41:53 +0330

    #467 db migration

---
+++ RMuseum/Migrations/20250326111037_CatCorrectionPlusPageId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatCorrectionPlusPageId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PageId",
+                table: "GanjoorCatCorrections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageId",
+                table: "GanjoorCatCorrections");
+        }
+    }
+}
