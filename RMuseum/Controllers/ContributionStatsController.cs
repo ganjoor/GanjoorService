3d8d3a5574583eb7789e91f846b6b8d9a7d8edff
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-03-27 12:13:34 +0330

    #478 /api/contributions/section

--- RMuseum/Controllers/ContributionStatsController.cs
+++ RMuseum/Controllers/ContributionStatsController.cs
@@ -92,6 +92,82 @@ namespace RMuseum.Controllers
             return Ok(result);
         }
 
+
+        /// <summary>
+        /// approved section edits daily
+        /// </summary>
+        /// <param name="paging"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet("section/corrections/daily")]
+        [AllowAnonymous]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<GroupedByDateViewModel>))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> GetApprovedSectionEditsGroupedByDateAsync([FromQuery] PagingParameterModel paging, Guid? userId = null)
+        {
+            var cacheKey = $"section/corrections/daily/{DateTime.Now.Date}/{paging.PageSize}/{paging.PageNumber}/{userId ?? Guid.Empty}";
+            if (!_memoryCache.TryGetValue(cacheKey, out (PaginationMetadata PagingMeta, GroupedByDateViewModel[] Tracks) pagedResult))
+            {
+                var res = await _service.GetApprovedSectionEditsGroupedByDateAsync(paging, userId);
+                if (!string.IsNullOrEmpty(res.ExceptionString))
+                    return BadRequest(res.ExceptionString);
+                pagedResult = res.Result;
+            }
+            // Paging Header
+            HttpContext.Response.Headers.Append("paging-headers", JsonConvert.SerializeObject(pagedResult.PagingMeta));
+
+            return Ok(pagedResult.Tracks);
+        }
+
+        /// <summary>
+        /// approved section edits grouped by user
+        /// </summary>
+        /// <param name="paging"></param>
+        /// <param name="day"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet("section/corrections/by/user")]
+        [AllowAnonymous]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<GroupedByUserViewModel>))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> GetApprovedSectionEditsGroupedByUserAsync([FromQuery] PagingParameterModel paging, DateTime? day, Guid? userId)
+        {
+            var cacheKey = $"section/corrections/by/user/{(day ?? DateTime.Now).Date}/{paging.PageSize}/{paging.PageNumber}/{userId ?? Guid.Empty}";
+            if (!_memoryCache.TryGetValue(cacheKey, out (PaginationMetadata PagingMeta, GroupedByUserViewModel[] Tracks) pagedResult))
+            {
+                var res = await _service.GetApprovedSectionEditsGroupedByUserAsync(paging, day, userId);
+                if (!string.IsNullOrEmpty(res.ExceptionString))
+                    return BadRequest(res.ExceptionString);
+                pagedResult = res.Result;
+            }
+
+            // Paging Header
+            HttpContext.Response.Headers.Append("paging-headers", JsonConvert.SerializeObject(pagedResult.PagingMeta));
+
+            return Ok(pagedResult.Tracks);
+        }
+
+        /// <summary>
+        /// summed up stats of approved section corrections
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("section/corrections")]
+        [AllowAnonymous]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(SummedUpViewModel))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> GetApprrovedSedtionEditsSummedUpStatsAsync()
+        {
+            if (!_memoryCache.TryGetValue($"section/corrections/{DateTime.Now.Date}", out SummedUpViewModel result))
+            {
+                var res = await _service.GetApprrovedSedtionEditsSummedUpStatsAsync();
+                if (!string.IsNullOrEmpty(res.ExceptionString))
+                    return BadRequest(res.ExceptionString);
+                result = res.Result;
+            }
+            return Ok(result);
+        }
+
+
         /// <summary>
         /// service
         /// </summary>
