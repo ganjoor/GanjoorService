1ff879ac24016640ff7771729f2f4d2922557a90
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-03-28 16:05:22 +0330

    #478 add publication date for poet spec lines and pictures

---
+++ RMuseum/Migrations/20250328123419_SpecPublicationDates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecPublicationDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublicationDate",
+                table: "GanjoorPoetSuggestedSpecLines",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublicationDate",
+                table: "GanjoorPoetSuggestedPictures",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicationDate",
+                table: "GanjoorPoetSuggestedSpecLines");
+
+            migrationBuilder.DropColumn(
+                name: "PublicationDate",
+                table: "GanjoorPoetSuggestedPictures");
+        }
+    }
+}
