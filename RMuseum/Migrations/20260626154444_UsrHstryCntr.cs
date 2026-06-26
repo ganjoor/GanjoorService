09f80394ed23556632f63bf72dfc5f18a527ba53
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2026-06-26 19:15:29 +0330

    #493 db migration

---
+++ RMuseum/Migrations/20260626154444_UsrHstryCntr.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsrHstryCntr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Counter",
+                table: "GanjoorUserPoemVisits",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Counter",
+                table: "GanjoorUserPoemVisits");
+        }
+    }
+}
