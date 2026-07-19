513b10df9ea34f17b9c478a7cb6b53e47d9e51bc
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2026-07-19 17:07:20 +0330

    warnings fix

--- GanjooRazor/Utils/EncDecUtil.cs
+++ GanjooRazor/Utils/EncDecUtil.cs
@@ -17,7 +17,7 @@ namespace GanjooRazor.Utils
             byte[] passwordBytes = Encoding.UTF8.GetBytes(pwd);
 
             // Hash the password with SHA256
-            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+            passwordBytes = SHA256.HashData(passwordBytes);
 
             // Generating salt bytes
             byte[] saltBytes = _GetRandomBytes();
@@ -44,7 +44,7 @@ namespace GanjooRazor.Utils
             byte[] passwordBytes = Encoding.UTF8.GetBytes(pwd);
 
             // Hash the password with SHA256
-            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+            passwordBytes = SHA256.HashData(passwordBytes);
 
             byte[] decryptedBytes = _AES_Decrypt(bytesToBeDecrypted, passwordBytes);
 
@@ -65,7 +65,7 @@ namespace GanjooRazor.Utils
         {
             int _saltSize = 4;
             byte[] ba = new byte[_saltSize];
-            RandomNumberGenerator.Create().GetBytes(ba);
+            RandomNumberGenerator.Fill(ba);
             return ba;
         }
 
@@ -78,25 +78,26 @@ namespace GanjooRazor.Utils
             byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             using (MemoryStream ms = new MemoryStream())
+            using (Aes aes = Aes.Create())
             {
-                using (var AES = Aes.Create("AesManaged"))
+                aes.KeySize = 256;
+                aes.BlockSize = 128;
+
+                // Use the static Pbkdf2 method
+                byte[] key = Rfc2898DeriveBytes.Pbkdf2(passwordBytes, saltBytes, 1000, HashAlgorithmName.SHA1, aes.KeySize / 8 + aes.BlockSize / 8);
+
+                // Extract key and IV from the derived bytes
+                aes.Key = key[0..(aes.KeySize / 8)];
+                aes.IV = key[(aes.KeySize / 8)..];
+
+                aes.Mode = CipherMode.CBC;
+
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
                 {
-                    AES.KeySize = 256;
-                    AES.BlockSize = 128;
-
-                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
-                    AES.Key = key.GetBytes(AES.KeySize / 8);
-                    AES.IV = key.GetBytes(AES.BlockSize / 8);
-
-                    AES.Mode = CipherMode.CBC;
-
-                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
-                        cs.Close();
-                    }
-                    encryptedBytes = ms.ToArray();
+                    cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                    cs.Close();
                 }
+                encryptedBytes = ms.ToArray();
             }
 
             return encryptedBytes;
@@ -111,25 +112,26 @@ namespace GanjooRazor.Utils
             byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             using (MemoryStream ms = new MemoryStream())
+            using (Aes aes = Aes.Create())
             {
-                using (var AES = Aes.Create("AesManaged"))
+                aes.KeySize = 256;
+                aes.BlockSize = 128;
+
+                // Use the static Pbkdf2 method
+                byte[] key = Rfc2898DeriveBytes.Pbkdf2(passwordBytes, saltBytes, 1000, HashAlgorithmName.SHA1, aes.KeySize / 8 + aes.BlockSize / 8);
+
+                // Extract key and IV from the derived bytes
+                aes.Key = key[0..(aes.KeySize / 8)];
+                aes.IV = key[(aes.KeySize / 8)..];
+
+                aes.Mode = CipherMode.CBC;
+
+                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
                 {
-                    AES.KeySize = 256;
-                    AES.BlockSize = 128;
-
-                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
-                    AES.Key = key.GetBytes(AES.KeySize / 8);
-                    AES.IV = key.GetBytes(AES.BlockSize / 8);
-
-                    AES.Mode = CipherMode.CBC;
-
-                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
-                        cs.Close();
-                    }
-                    decryptedBytes = ms.ToArray();
+                    cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                    cs.Close();
                 }
+                decryptedBytes = ms.ToArray();
             }
 
             return decryptedBytes;
