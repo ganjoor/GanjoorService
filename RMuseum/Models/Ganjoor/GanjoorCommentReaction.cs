f6b0886c6cc7a9e70a3fb8336102a255108b0a44
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2026-07-21 21:15:29 +0330

    #495 GanjoorCommentReaction model

---
+++ RMuseum/Models/Ganjoor/GanjoorCommentReaction.cs
@@ -0,0 +1,24 @@
+﻿using RSecurityBackend.Models.Auth.Db;
+using System;
+
+namespace RMuseum.Models.Ganjoor
+{
+    public class GanjoorCommentReaction
+    {
+        public int Id { get; set; }
+
+        public int GanjoorCommentId { get; set; }
+        public virtual GanjoorComment GanjoorComment { get; set; }
+
+        public Guid UserId { get; set; }
+        public virtual RAppUser User { get; set; }
+
+        /// <summary>
+        /// +1 = Like
+        /// -1 = Dislike
+        /// </summary>
+        public short Value { get; set; }
+
+        public DateTime ReactionDate { get; set; }
+    }
+}
